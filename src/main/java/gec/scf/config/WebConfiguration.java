a899bb8ada814a843f098fcc5571ed4369bcd465
Natthapong Noosing <natthapong.noo@gec.co.th>
2016-03-25 17:45:49 +0700

    [ADD] Create Loan Request - Validate and Submit Page

--- src/main/java/gec/scf/config/WebConfiguration.java
+++ src/main/java/gec/scf/config/WebConfiguration.java
@@ -1,19 +1,16 @@
 package gec.scf.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
-	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-	        "classpath:/META-INF/resources/", "classpath:/resources/",
-	        "classpath:/static/", "classpath:/public/" };
-	
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/META-INF/resources/",
+			"classpath:/resources/", "classpath:/static/", "classpath:/public/" };
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
@@ -23,6 +20,6 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		 registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
 	}
 }
---
+++ src/main/java/gec/scf/web/package-info.java
@@ -0,0 +1,1 @@
+package gec.scf.web;
