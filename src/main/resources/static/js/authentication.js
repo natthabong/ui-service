4f313bc2e8c8e9defbc384ff05c83cbe192fba46
Natthapong Noosing <natthapong.noo@gec.co.th>
2016-04-01 16:15:40 +0700

    [FIXED] Http header for login

--- src/main/resources/static/js/authentication.js
+++ src/main/resources/static/js/authentication.js
@@ -1,6 +1,17 @@
 (function () {
     'use strict';
-    var app = angular.module('authenApp', ['ngCookies']);
+    var app = angular.module('authenApp', ['ngCookies']).config(function ($httpProvider) {
+        
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+        $httpProvider.defaults.headers.common['Accept'] = 'application/json';
+        $httpProvider.defaults.transformRequest = function (data) {
+            if (data === undefined) {
+                return data;
+            }
+            return $.param(data);
+        }
+    });
     app.controller('LoginController', ['$location', 'AuthenticationService', function ($location, AuthenticationService) {
         var self = this;
 
@@ -14,6 +25,7 @@
         function login() {
             self.dataLoading = true;
             AuthenticationService.Login(self.username, self.password, function (response) {
+                console.log(response);
                 if (response.success) {
                     AuthenticationService.SetCredentials(self.username, self.password);
                     $location.path('/');
@@ -38,7 +50,7 @@
 
             /* Use this for real authentication
              ----------------------------------------------*/
-            $http.post('/api/authenticate', {
+            $http.post('http://gecscf.com:9002/api/authenticate', {
                     username: username,
                     password: password
                 })
@@ -50,7 +62,7 @@
 
         function SetCredentials(username, password) {
             var authdata = btoa(username + ':' + password);
-
+            console.log(authdata);
             $rootScope.globals = {
                 currentUser: {
                     username: username,
@@ -65,8 +77,89 @@
         function ClearCredentials() {
             $rootScope.globals = {};
             $cookieStore.remove('globals');
-            $http.defaults.headers.common.Authorization = 'Basic';
+            delete $http.defaults.headers.common.Authorization;
         }
     }]);
 
+    // Base64 encoding service used by AuthenticationService
+    var Base64 = {
+
+        keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                    this.keyStr.charAt(enc1) +
+                    this.keyStr.charAt(enc2) +
+                    this.keyStr.charAt(enc3) +
+                    this.keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (base64test.exec(input)) {
+                window.alert("There were invalid base64 characters in the input text.\n" +
+                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                    "Expect errors in decoding.");
+            }
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            do {
+                enc1 = this.keyStr.indexOf(input.charAt(i++));
+                enc2 = this.keyStr.indexOf(input.charAt(i++));
+                enc3 = this.keyStr.indexOf(input.charAt(i++));
+                enc4 = this.keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+
+            } while (i < input.length);
+
+            return output;
+        }
+    };
 })();
