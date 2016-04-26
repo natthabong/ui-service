675ae1d58126cd0f6aad0f782bfc9f79003b2bdf
thanunchai.kun <thanunchai.kun@gec.co.th>
2016-04-26 15:05:47 +0700

    [NEW] Add page for confirm

--- src/main/java/gec/scf/upload/UploadController.java
+++ src/main/java/gec/scf/upload/UploadController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import gec.scf.util.AjaxUtils;
 
 @Controller
+@RequestMapping(path="/upload")
 public class UploadController {
 	private String UPLOAD_DOCUMENT_URL = "upload/upload_document";
 	private String UPLOAD_DOCUMENT_HISTORY = "upload/document_upload_history";
+	private String UPLOAD_DOCUMENT_VERIFY = "upload/verify_document_upload";
 	
 	//@PreAuthorize("isAuthenticated()")
-	@RequestMapping(path={"/upload/doc"}, method = RequestMethod.GET)
+	@RequestMapping(path={"/doc"}, method = RequestMethod.GET)
 	public String uploadDoc(HttpServletRequest req){
 		String requestedWith = req.getHeader("X-Requested-With");
 		if (AjaxUtils.isAjaxRequest(requestedWith)) {
@@ -23,7 +25,7 @@ public class UploadController {
 		return UPLOAD_DOCUMENT_URL;
 	}
 	//@PreAuthorize("isAuthenticated()")	
-	@RequestMapping(path="/upload/doc/history", method = RequestMethod.GET)
+	@RequestMapping(path="/doc/history", method = RequestMethod.GET)
 	public String uploadDocHistory(HttpServletRequest req){
 		String requestedWith = req.getHeader("X-Requested-With");
 		if(AjaxUtils.isAjaxRequest(requestedWith)){
@@ -31,4 +33,14 @@ public class UploadController {
 		}
 		return UPLOAD_DOCUMENT_HISTORY;
 	}
+	
+	//@PreAuthorize("isAuthenticated()")	
+		@RequestMapping(path="/doc/verify", method = RequestMethod.GET)
+		public String uploadDocVerify(HttpServletRequest req){
+			String requestedWith = req.getHeader("X-Requested-With");
+			if(AjaxUtils.isAjaxRequest(requestedWith)){
+				return UPLOAD_DOCUMENT_VERIFY.concat(" :: content");
+			}
+			return UPLOAD_DOCUMENT_VERIFY;
+		}
 }
