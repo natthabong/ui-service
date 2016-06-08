9a56049b9718c7c45346d7a608e15f240e690760
Natthapong Noosing <natthapong.noo@gec.co.th>
2016-06-08 14:57:59 +0700

    [NEW] Verify Transaction

---
+++ src/main/resources/static/js/app/verify-transactions/verify-transaction-controller.js
@@ -0,0 +1,9 @@
+angular.module('scfApp').controller('VerifyTransactionController',
+		[ 'VerifyTransactionService', function(VerifyTransactionService) {
+			var vm = this;
+
+			vm.searchTransaction = function() {
+
+			};
+
+		} ]);
