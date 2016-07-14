9f55ec355af2ea84193ba0d1ccfee2f580b6ba17
thanunchai.kun <thanunchai.kun@gec.co.th>
2016-07-14 16:58:20 +0700

    [NEW] Dashboard display

---
+++ src/main/resources/static/js/app/dashboard/newduedate-group-controller.js
@@ -0,0 +1,6 @@
+angular.module('scfApp').controller('NewduedateGroupController', ['$scope',function($scope){
+	console.log($scope.$parent.dashboardItem);
+	this.name = 'hello';
+//	var vm = this;
+//	vm.helloworld = 'Hello world';
+}]);
