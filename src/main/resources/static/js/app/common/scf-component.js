2820326826dcb4b532baeb99c7cc82b36d69e8eb
Natthapong Noosing <natthapong.noo@gec.co.th>
2016-07-15 10:27:40 +0700

    [FIXED] Bug label id

--- src/main/resources/static/js/app/common/scf-component.js
+++ src/main/resources/static/js/app/common/scf-component.js
@@ -380,80 +380,80 @@
                     '</a>'
             };
         }])
-//        .directive('scfPagination', [function() {
-//            return {
-//                restrict: 'AE',
-//                replace: true,
-//                scope: {
-//                    currentPage: '=',
-//                    pageSizeModel: '=',
-//                    pageSizeList: '<',
-//                    totalPage: '=',
-//                    pageAction: '='
-//                },
-//                link: fieldLink,
-//                template: fieldTemplate
-//
-//            };
-//
-//            function fieldLink(scope, element, attrs) {
-//
-//                scope.$watch('[totalPage, currentPage]', function(value) {
-//                    disableButton(scope, element);
-//                });
-//
-//                scope.scfPaginationAction = function(btnAction) {
-//                    var pageModel = {
-//                        page: scope.currentPage,
-//                        pageSize: scope.pageSizeModel
-//                    };
-//                    if (btnAction === 'first' || btnAction === 'changeSize') {
-//                        pageModel.page = 0;
-//                    } else if (btnAction === 'back') {
-//                        pageModel.page += -1;
-//                    } else if (btnAction === 'next') {
-//                        pageModel.page += 1;
-//                    } else if (btnAction === 'last') {
-//                        pageModel.page = scope.totalPage - 1;
-//                    }
-//                    scope.pageAction(pageModel);
-//                };
-//
-//                if (attrs.dropdownId != undefined) {
-//                    element[0].children[2].children[0].id = attrs.dropdownId
-//                }
-//            }
-//
-//            function disableButton(scope, element) {
-//                var totalPage = +scope.totalPage;
-//                var currentPage = +scope.currentPage;
-//                /* check is first page*/
-//                if (currentPage === 0) {
-//                    /* disable button First, Back page */
-//                    element[0].children[0].children[0].disabled = true;
-//                    element[0].children[1].children[0].disabled = true;
-//                } else {
-//                    /* enable button First, Back page*/
-//                    element[0].children[0].children[0].disabled = false;
-//                    element[0].children[1].children[0].disabled = false;
-//                }
-//
-//                if (currentPage === (totalPage - 1)) {
-//                    /* disable button Next, Last page */
-//                    element[0].children[3].children[0].disabled = true;
-//                    element[0].children[4].children[0].disabled = true;
-//                } else {
-//                    /* enable button Next, Last page */
-//                    element[0].children[3].children[0].disabled = false;
-//                    element[0].children[4].children[0].disabled = false;
-//                }
-//            }
-//
-//            function fieldTemplate(element, attrs) {
-//                var template = '<ul class="scf-paging form-inline">' + '<li><scf-button type="button" ng-click="scfPaginationAction(\'first\')" class="btn-sm" id="first-page-button"><span class="glyphicon glyphicon-step-backward" aria-hidden="true"></span></scf-button></li>' + '<li><scf-button type="button" ng-click="scfPaginationAction(\'back\')" class="btn-sm" id="back-page-button"><span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span></scf-button></li>' + '<li><scf-dropdown ng-model="pageSizeModel" ng-change="scfPaginationAction(\'changeSize\')" component-data="pageSizeList"></scf-dropdown</li>' + '<li><button type="button" ng-click="scfPaginationAction(\'next\')" class="btn btn-default btn-sm" id="next-page-button"><span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span></button></li>' + '<li><button type="button" ng-click="scfPaginationAction(\'last\')" class="btn btn-default btn-sm" id="last-page-button"><span class="glyphicon glyphicon-step-forward" aria-hidden="true"></span></button></li>' + '</ul>';
-//                return template;
-//            }
-//        }])
+        .directive('scfPagination', [function() {
+            return {
+                restrict: 'AE',
+                replace: true,
+                scope: {
+                    currentPage: '=',
+                    pageSizeModel: '=',
+                    pageSizeList: '<',
+                    totalPage: '=',
+                    pageAction: '='
+                },
+                link: fieldLink,
+                template: fieldTemplate
+
+            };
+
+            function fieldLink(scope, element, attrs) {
+
+                scope.$watch('[totalPage, currentPage]', function(value) {
+                    disableButton(scope, element);
+                });
+
+                scope.scfPaginationAction = function(btnAction) {
+                    var pageModel = {
+                        page: scope.currentPage,
+                        pageSize: scope.pageSizeModel
+                    };
+                    if (btnAction === 'first' || btnAction === 'changeSize') {
+                        pageModel.page = 0;
+                    } else if (btnAction === 'back') {
+                        pageModel.page += -1;
+                    } else if (btnAction === 'next') {
+                        pageModel.page += 1;
+                    } else if (btnAction === 'last') {
+                        pageModel.page = scope.totalPage - 1;
+                    }
+                    scope.pageAction(pageModel);
+                };
+
+                if (attrs.dropdownId != undefined) {
+                    element[0].children[2].children[0].id = attrs.dropdownId
+                }
+            }
+
+            function disableButton(scope, element) {
+                var totalPage = +scope.totalPage;
+                var currentPage = +scope.currentPage;
+                /* check is first page*/
+                if (currentPage === 0) {
+                    /* disable button First, Back page */
+                    element[0].children[0].children[0].disabled = true;
+                    element[0].children[1].children[0].disabled = true;
+                } else {
+                    /* enable button First, Back page*/
+                    element[0].children[0].children[0].disabled = false;
+                    element[0].children[1].children[0].disabled = false;
+                }
+
+                if (currentPage === (totalPage - 1)) {
+                    /* disable button Next, Last page */
+                    element[0].children[3].children[0].disabled = true;
+                    element[0].children[4].children[0].disabled = true;
+                } else {
+                    /* enable button Next, Last page */
+                    element[0].children[3].children[0].disabled = false;
+                    element[0].children[4].children[0].disabled = false;
+                }
+            }
+
+            function fieldTemplate(element, attrs) {
+                var template = '<ul class="scf-paging form-inline">' + '<li><scf-button type="button" ng-click="scfPaginationAction(\'first\')" class="btn-sm" id="first-page-button"><span class="glyphicon glyphicon-step-backward" aria-hidden="true"></span></scf-button></li>' + '<li><scf-button type="button" ng-click="scfPaginationAction(\'back\')" class="btn-sm" id="back-page-button"><span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span></scf-button></li>' + '<li><scf-dropdown ng-model="pageSizeModel" ng-change="scfPaginationAction(\'changeSize\')" component-data="pageSizeList"></scf-dropdown</li>' + '<li><button type="button" ng-click="scfPaginationAction(\'next\')" class="btn btn-default btn-sm" id="next-page-button"><span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span></button></li>' + '<li><button type="button" ng-click="scfPaginationAction(\'last\')" class="btn btn-default btn-sm" id="last-page-button"><span class="glyphicon glyphicon-step-forward" aria-hidden="true"></span></button></li>' + '</ul>';
+                return template;
+            }
+        }])
         .directive('scfModal', [function() {
             return {
                 template: '<div class="modal" data-keyboard="false" data-backdrop="static">' +
