c2aca5825e9f994c6e5f3eea282936b00bfedc83
Natthapong Noosing <natthapong.noo@gec.co.th>
2016-07-26 17:13:07 +0700

    [NEW] Credit Information dashboard

---
+++ src/main/resources/static/js/app/dashboard/credit-information-controller.js
@@ -0,0 +1,42 @@
+angular.module('scfApp').controller(
+		'CreditInformationDashboardController',
+		[
+				'$log',
+				'$scope',
+				'$state',
+				'$stateParams',
+				'$timeout',
+				'PageNavigation',
+				'Service',
+				function($log, $scope, $state, $stateParams, $timeout,
+						PageNavigation, Service) {
+					var vm = this;
+					var log = $log;
+					vm.index = 0;
+					var dataSource = Service
+					.requestURL('/api/credit-information/get', {});
+					
+					dataSource.promise.then(function(response) {
+		                vm.data = response;
+		                i = 0;
+		                angular.forEach(vm.data, function(value, idx) {
+		                	if(vm.isSameAccount(value.accountId, vm.data, idx)){
+		                		value.rowNo = ++i;
+		                		value.showAccountFlag = true;
+		                	}
+		                });
+		            }).catch();
+					
+					vm.decodeBase64 = function(data){
+						return atob(data);
+					}
+					
+					vm.isSameAccount = function(accountId, data, index){
+						if(index == 0 ){
+							return true;
+						}
+						else{
+							return accountId != data[index-1].accountId;
+						}
+					}
+				}]);
