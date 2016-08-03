fdbad0c04be3914686856285e6e45cd361a23fd7
Natthapong Noosing <natthapong.noo@gec.co.th>
2016-08-03 10:25:56 +0700

    [NEW] Configuration for fixed header

--- src/main/java/gec/scf/web/HomeController.java
+++ src/main/java/gec/scf/web/HomeController.java
@@ -2,30 +2,35 @@ package gec.scf.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import gec.scf.util.AjaxUtils;
 
 @Controller
 public class HomeController {
+
 	private String HOME_VIEW_NAME = "index";
-	
+
+	@Value(value = "${scf.ui.header.fixed:true}")
+	private boolean fixedHeader;
+
 	@PreAuthorize("isAuthenticated()")
-	@RequestMapping(path={"/", "/home"}, method = RequestMethod.GET)
-	public String home(HttpServletRequest req) {
+	@RequestMapping(path = { "/", "/home" }, method = RequestMethod.GET)
+	public ModelAndView home(HttpServletRequest req) {
+		String view = null;
 		String requestedWith = req.getHeader("X-Requested-With");
 		if (AjaxUtils.isAjaxRequest(requestedWith)) {
-			return HOME_VIEW_NAME.concat(" :: content");
+			view = HOME_VIEW_NAME.concat(" :: content");
 		}
-		return HOME_VIEW_NAME;	
+		else {
+			view = HOME_VIEW_NAME;
+		}
+		return new ModelAndView(view, "fixedHeader", fixedHeader);
 	}
-	
-	// Match everything without a suffix (so not a static resource)
-//	@RequestMapping(value = "/{[path:[^\\.]*}")
-//	public String prefix(){
-//		return "forward:/";
-//	}
+
 }
