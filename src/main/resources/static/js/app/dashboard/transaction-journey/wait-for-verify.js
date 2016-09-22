61f346f3e156b0ffe8b751edcbf582082422f249
natrada.kae <natrada.kae@gec.co.th>
2016-09-22 13:19:39 +0700

    [FIXED] modify UI for support call API and send statusCode.

--- src/main/resources/static/js/app/dashboard/transaction-journey/wait-for-verify.js
+++ src/main/resources/static/js/app/dashboard/transaction-journey/wait-for-verify.js
@@ -1,42 +1,59 @@
-angular.module('scfApp').controller('JourneyWaitForVerifyController', ['$scope', 'Service', 'PageNavigation', '$cookieStore', '$state', 'SCFCommonService', function($scope, Service, PageNavigation, $cookieStore, $state, SCFCommonService) {
-    var vm = this;
-    var compositParent = $scope.$parent;
-    var dashboarParent = compositParent.$parent.it;
-    var dahsboarItemParent = dashboarParent.dashboardItem;
-    vm.headerLabel = dahsboarItemParent.headerLabel;
-    var listStoreKey = 'listrancri';
-    vm.waitForVerifyModel = {
-        totalTransaction: 50,
-        totalAmount: '15M',
-        maxAge: '2'
-    };
-    vm.listTransactionModel = {
-        dateType: 'transactionDate',
-        dateFrom: '',
-        dateTo: '',
-        sponsorId: '',
-        supplierId: '',
-        statusGroup: 'INTERNAL_STEP',
-        order: '',
-        orderBy: '',
-        statusCode: 'WAIT_FOR_VERIFY'
-    };
+angular.module('scfApp').controller(
+		'JourneyWaitForVerifyController', 
+		[
+		 	'$scope', 'Service', 'PageNavigation', '$cookieStore', '$state', 'SCFCommonService', 
+		 	function($scope, Service, PageNavigation, $cookieStore, $state, SCFCommonService) {	
+			    var vm = this;
+			    var compositParent = $scope.$parent;
+			    var dashboardParent = compositParent.$parent.it;
+			    var dashboardItemParent = dashboardParent.dashboardItem;
+			    vm.headerLabel = dashboardItemParent.headerLabel;
+			    var listStoreKey = 'listrancri';
+			    
+			    vm.waitForVerifyModel = {
+			        totalTransaction: 0,
+			        totalAmount: '',
+			        maxAge: ''
+			    };
+			    
+			    vm.listTransactionModel = {
+			        dateType: 'transactionDate',
+			        dateFrom: '',
+			        dateTo: '',
+			        sponsorId: '',
+			        supplierId: '',
+			        statusGroup: 'INTERNAL_STEP',
+			        order: '',
+			        orderBy: '',
+			        statusCode: 'WAIT_FOR_VERIFY'
+			    };
+			
+			    var filterStatusCode = splitFilterStatusCode(dashboardParent.filterItems);
+			    
+			    function splitFilterStatusCode(data){
+					var filterItem = data.split(":");
+					return filterItem[1];
+				}
+			    
+			    vm.load = function() {
+			    	var newDocumentDeferred = Service.requestURL('/api/summary-transaction/get', filterStatusCode, 'POST');
+			    	newDocumentDeferred.promise.then(function(response){
+			    		vm.waitForVerifyModel.totalTransaction = response.totalTransaction;
+			    		vm.waitForVerifyModel.totalAmount = SCFCommonService.shortenLargeNumber(response.totalAmount);
+			    		vm.waitForVerifyModel.maxAge = SCFCommonService.shortenLargeNumber(response.maxAge);
+			    	}).catch(function(response){
+			    		
+			    	});
+			    }
+			    	
+			    vm.load();
+			    
+			    vm.transactionList = function() {
+			        $cookieStore.put(listStoreKey, vm.listTransactionModel);
+			        $state.go('/transaction-list', {
+			            backAction: true
+			        });
+			
+			    }
 
-
-    //var newDocumentDeferred = Service.requestURL('/api/');
-    //	newDocumentDeferred.promise.then(function(response){
-    //		vm.waitForVerifyModel.totalTransaction = response.totalTransaction;
-    //			vm.waitForVerifyModel.totalAmount = SCFCommonService.shortenLargeNumber(response.totalAmount);
-    //			vm.waitForVerifyModel.maxAge = SCFCommonService.shortenLargeNumber(response.maxAge);
-    //	}).catch(function(response){
-    //		
-    //	});
-    vm.transactionList = function() {
-        $cookieStore.put(listStoreKey, vm.listTransactionModel);
-        $state.go('/transaction-list', {
-            backAction: true
-        });
-
-    }
-
-}]);
+		 	}]);
