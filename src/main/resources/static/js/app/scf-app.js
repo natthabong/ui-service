ba1382e9886d231e931d0a047f4e9d797587a98a
kowit.tip <kowit.tip@gec.co.th>
2016-11-21 15:47:34 +0700

    [FIXED] profile-controller.js add config to scf-app.js

--- src/main/resources/static/js/app/scf-app.js
+++ src/main/resources/static/js/app/scf-app.js
@@ -151,7 +151,7 @@ var app = angular.module('scfApp', ['pascalprecht.translate', 'ui.router', 'ui.b
 				controllerAs: 'sponsorConfigCtrl',
 				templateUrl: '/sponsor-configuration',
 				params: { organizeModel: null, fileLayoutModel: null},
-				resolve: load(['js/app/sponsor-configuration/sponsor-config-controller.js', 'js/app/sponsor-configuration/file-layouts-controller.js', 'js/app/sponsor-configuration/customer-code-groups-controller.js','js/app/common/scf-component.js', 'js/app/common/scf-component.css'])
+				resolve: load(['js/app/sponsor-configuration/sponsor-config-controller.js', 'js/app/sponsor-configuration/profile-controller.js', 'js/app/sponsor-configuration/file-layouts-controller.js', 'js/app/sponsor-configuration/customer-code-groups-controller.js','js/app/common/scf-component.js', 'js/app/common/scf-component.css'])
 			}).state('/sponsor-configuration/file-layouts/new-file-layout',{
 				url: '/sponsor-configuration/file-layouts/new-file-layout',
 				controller: 'NewFileLayoutController',
