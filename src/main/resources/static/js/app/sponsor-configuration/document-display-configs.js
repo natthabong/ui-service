a22a4bc8d9c428cc09531ed787cd83db5905a3fc
GEC\thanunchai.kun <thanunchai.kun@gec.co.th>
2016-11-29 14:06:51 +0700

    [FIXED] แก้ไขให้ call service ด้วย function get

--- src/main/resources/static/js/app/sponsor-configuration/document-display-configs.js
+++ src/main/resources/static/js/app/sponsor-configuration/document-display-configs.js
@@ -2,7 +2,8 @@ angular.module('scfApp').controller('DocumentDisplayConfigController', ['Service
     '$log',
     '$scope',
     'PageNavigation',
-    function(Service, $log, $scope, PageNavigation) {
+    'SCFCommonService',
+    function(Service, $log, $scope, PageNavigation, SCFCommonService) {
         var vm = this;
         var log = $log;
 
@@ -41,31 +42,34 @@ angular.module('scfApp').controller('DocumentDisplayConfigController', ['Service
                 label: '',
                 cssTemplate: 'text-center',
                 sortData: false,
-                cellTemplate: '<scf-button id="document-display-{{data.documentDisplayId}}-setup-button" class="btn-default gec-btn-action" ng-click="docDisplayCtrl.setupDisplayDocument(data)" title="Config a file layout" ng-hide="!data.completed"><i class="fa fa-cog fa-lg" aria-hidden="true"></i></scf-button>' +
-                    '<scf-button id="document-display-{{data.documentDisplayId}}-warning-setup-button" class="btn-default gec-btn-action" ng-click="docDisplayCtrl.setupDisplayDocument(data)" title="Config a file layout" ng-hide="data.completed"><img ng-hide="data.completed" data-ng-src="img/gear_warning.png" style="height: 13px; width: 14px;"/></scf-button>'
+                cellTemplate: '<scf-button id="displays-{{data.documentDisplayId}}-setup-button" class="btn-default gec-btn-action" ng-click="docDisplayCtrl.setupDisplayDocument(data)" title="Config a file layout" ng-hide="!data.completed"><i class="fa fa-cog fa-lg" aria-hidden="true"></i></scf-button>' +
+                    '<scf-button id="displays-{{data.documentDisplayId}}-warning-setup-button" class="btn-default gec-btn-action" ng-click="docDisplayCtrl.setupDisplayDocument(data)" title="Config a file layout" ng-hide="data.completed"><img ng-hide="data.completed" data-ng-src="img/gear_warning.png" style="height: 13px; width: 14px;"/></scf-button>'
 
             }]
         };
 
-        //        vm.data = [{
-        //            displayName: 'AP document display',
-        //            documentDisplayId: 1,
-        //            completed: false
-        //        }];
-        vm.data = [];
+        //        vm.data = [];
+        vm.data = [{
+            displayName: 'AP document display',
+            documentDisplayId: 1,
+            completed: false
+        }, {
+            displayName: 'AP document display',
+            documentDisplayId: 2,
+            completed: false
+        }];
 
         vm.splitePageTxt = '0 - 0 of 0';
         vm.loadDocumentDisplayConfig = function() {
             var requestUrl = '/api/v1/organize-customers/' + $scope.sponsorId + '/sponsor-configs/SFP/displays';
 
-            var docDisplayConfDiferred = Service.requestURL(requestUrl, {
-                    offset: 0,
-                    limit: 20
-                },
-                'GET');
+            var docDisplayConfDiferred = Service.doGet(requestUrl, {
+                offset: 0,
+                limit: 20
+            });
 
             docDisplayConfDiferred.promise.then(function(response) {
-                vm.data = response;
+                vm.data = response.data;
                 vm.pageModel.totalRecord = response.headers('X-Total-Count');
                 vm.pageModel.totalPage = response.headers('X-Total-Page');
                 vm.splitePageTxt = SCFCommonService.splitePage(vm.pageModel.pageSizeSelectModel, vm.pageModel.page, vm.pageModel.totalRecord);
