b9f7803f04cb86055b4ecc9d1df519855909a404
natrada.kae <natrada.kae@gec.co.th>
2017-01-19 10:16:12 +0700

    [FIXED] changed logic of disabled text, dropdown.

--- src/main/resources/static/js/app/sponsor-configuration/payment-date-formulas/payment-date-formula-setting-controller.js
+++ src/main/resources/static/js/app/sponsor-configuration/payment-date-formulas/payment-date-formula-setting-controller.js
@@ -410,24 +410,10 @@ app.controller('NewPaymentPeriodController', [ '$scope', '$rootScope','Service',
             vm.dayOfWeekTypes.push(selectObj);
         });
     }
-	
-	vm.checkPeriodType = function() {
-		if(vm.period.paymentPeriodType==='EVERY_DAY'){
-			vm.disabledDateOfMonth = true;
-			vm.disabledDayOfWeek = true;
-		}else if(vm.period.paymentPeriodType==='DATE_OF_MONTH'){
-			vm.disabledDateOfMonth = false;
-			vm.disabledDayOfWeek = true;
-		}else if(vm.period.paymentPeriodType==='DAY_OF_WEEK'){
-			vm.disabledDateOfMonth = true;
-			vm.disabledDayOfWeek = false;
-		}
-	}
-	
+
 	vm.initLoad = function() {
 		loadOccurrenceWeek();
 		loadDayOfWeek();
-		vm.checkPeriodType();
 	}
 
 	vm.initLoad();
