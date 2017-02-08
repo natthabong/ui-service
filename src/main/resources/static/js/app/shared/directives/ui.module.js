fab01c71343b9e03fe8b0d728d3c4c34f72bb246
GEC\thanunchai.kun <thanunchai.kun@gec.co.th>
2017-02-08 19:03:44 +0700

    [FIXED] แก้ปัญหา suggestion

--- src/main/resources/static/js/app/shared/directives/ui.module.js
+++ src/main/resources/static/js/app/shared/directives/ui.module.js
@@ -6,12 +6,12 @@ angular.module('gecscf.ui', []).run([ "$templateCache", function($templateCache)
 		
 		$templateCache.put('ui/template/autoSuggest.html', 
 				'<input type="text" id="{{id}}" ng-disabled="{{disable}}" placeholder="{{model.placeholder}}"'+
-				' class="form-control" uib-typeahead="data for data in model.query($viewValue) |filter:$viewValue"'+
+				' class="form-control" uib-typeahead="data for data in model.query($viewValue)"'+
 				' ng-model="ngModel" typeahead-template-url="{{model.itemTemplateUrl}}"/>');
 		
 		$templateCache.put('ui/template/autoSuggestTemplate.html',
 				'<a>'+
-	      '<span >{{match}}</span></a>');
+	      '<span ng-bind-html="match.model.sponsorName | uibTypeaheadHighlight:query"></span></a>');
 
 	} ]) .factory('UIModelFactory', ['$q', '$timeout', function ($q, $timeout) {
 		
