4506168b16190e9ea9c204d5df627a40a13c5510
Natthapong Noosing <natthapong.noo@gec.co.th>
2017-03-21 15:55:56 +0700

    [NEW] User list.

--- src/main/resources/static/js/app/modules/user/user.module.js
+++ src/main/resources/static/js/app/modules/user/user.module.js
@@ -1,14 +1,6 @@
 'use strict';
 
-angular.module('gecscf.user', [ 'blockUI', 'ngDialog' ]).config(
-	[ 'ngDialogProvider', function(ngDialogProvider) {
-	    ngDialogProvider.setDefaults({
-		className : 'ngdialog-theme-default',
-		plain : false,
-		showClose : false,
-		closeByDocument : false,
-		closeByEscape : false,
-		appendTo : false,
-		disableAnimation : true
-	    });
+angular.module('gecscf.user', [ 'ui.router' ]).config(
+	[ '$stateProvider', function($stateProvider) {
+
 	} ]);
