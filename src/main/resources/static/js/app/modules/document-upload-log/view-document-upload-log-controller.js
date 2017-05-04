5230b4b55c9b728788458168b55b6649337c4c1b
natrada.kae <natrada.kae@gec.co.th>
2017-05-04 17:14:36 +0700

    [FIXED] add translation for view document upload log page.

--- src/main/resources/static/js/app/modules/document-upload-log/view-document-upload-log-controller.js
+++ src/main/resources/static/js/app/modules/document-upload-log/view-document-upload-log-controller.js
@@ -15,14 +15,27 @@ angular
 			'ngDialog',
 			'$q', 
 			'UIFactory', 
+			'PagingController', 
 			function(SCFCommonService, $log, $scope, $stateParams, $timeout, $rootScope,
-				PageNavigation, Service, $http, blockUI, ngDialog, $q, UIFactory) {
+				PageNavigation, Service, $http, blockUI, ngDialog, $q, UIFactory, PagingController) {
 		
 				var vm = this;
 				var log = $log;
-		
+				
+				vm.headerMsg = 'View document upload log';
+				
 				vm.roleType = $stateParams.roleType;
 				vm.documentUploadLogModel = $stateParams.documentUploadLogModel;
+				
+				var uploadLogId = vm.documentUploadLogModel.uploadDocumentLogId;
+				
+				console.log(vm.roleType);
+				console.log(vm.documentUploadLogModel);
+				console.log(vm.documentUploadLogModel.fileType);
+				
+				vm.sponsorName = '';
+				
+				vm.viewCriteria = {};
 				
 				vm.pageSizeList = [{
 			        label: '10',
@@ -54,18 +67,54 @@ angular
 			            field: 'description',
 			            label: 'Description',
 			            idValueField: '$rowNo',
-			            id: 'view-document-upload-log-description-{value}-label'
+			            id: 'view-document-upload-log-description-{value}-label',
 		            	sortData: false,
 		            	cssTemplate: 'text-left'
 			        }]
 			    }
 				
-				vm.pagingController = PagingController.create('api/v1/upload-logs/'+vm.documentUploadLogModel.uploadDocumentLogId+'/items', null, 'GET');
+				vm.getSponsorName = function(){
+					var serviceUrl = '/api/v1/organize-customers/'+vm.documentUploadLogModel.organizeId+'/profile';
+					var serviceDiferred = Service.doGet(serviceUrl, {});		
+					serviceDiferred.promise.then(function(response){
+						vm.organizeData = response.data;
+						vm.sponsorName = vm.organizeData.organizeName;
+						
+					}).catch(function(response){
+						log.error('Load organize data error');
+					});
+				}
+				
+				vm.pagingController = PagingController.create('api/v1/upload-logs/'+uploadLogId+'/items', vm.viewCriteria, 'GET');
 				
 				vm.searchListLog = function(pagingModel) {
 					var logListDiferred = vm.pagingController.search(pagingModel);
 				}
-				vm.searchListLog();
+				
+				vm.initLoad = function() {	
+					vm.getSponsorName();
+					vm.searchListLog();
+				}
+				
+				vm.downloadFile = function(){
+					$http({
+			             method: 'POST',
+			             url: '/api/v1/upload-logs/'+uploadLogId+'/download-source-file',
+			             data: transactionModel,
+			             responseType: 'arraybuffer'
+			         }).success(function(response) {
+			          var file = new Blob([response], {type: 'text/plain'});
+			          var fileURL = URL.createObjectURL(file);
+			          var a         = document.createElement('a');
+			             a.href        = fileURL; 
+			             a.target      = '_blank';
+			             a.download    = vm.documentUploadLogModel.fileName;
+			             document.body.appendChild(a);
+			             a.click();
+			         }).error(function(response) {
+			             
+			         });
+				}
 			}
 		]
 	);
