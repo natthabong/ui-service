52cda49062ac0ef8689967b729fe743f6cbec30c
GEC\cheerakwan.sut <cheerakwan@gmail.com>
2017-05-18 11:37:09 +0700

    [CHANGE] layout channel config

--- src/main/resources/static/js/app/sponsor-configuration/channel-config-controller.js
+++ src/main/resources/static/js/app/sponsor-configuration/channel-config-controller.js
@@ -41,11 +41,23 @@ angular
 							}
 
 							vm.editChannel = function(data) {
-								PageNavigation.gotoPage('/');
+								var params = {
+						            	selectedItem: data
+						            };
+						        PageNavigation.gotoPage('/sponsor-configuration/import-channels/settings', params);
 							}
 
 							vm.deleteChannel = function(data) {
 								PageNavigation.gotoPage('/');
+							}
+							
+							vm.disableTestConnection = function(data) {
+								if(data.channelName == 'Web'){
+									return true;
+								}else{
+									return false;
+								}
+								
 							}
 
 							vm.data = [];
@@ -57,19 +69,19 @@ angular
 										{
 											fieldName : 'channelName',
 											label : 'Channel',
-											id : 'channel-{value}-label',
+											id : 'channel-{value}',
 											cssTemplate : 'text-left',
 										},
 										{
 											fieldName : 'status',
 											label : 'Status',
-											id : 'channel-{value}-status-label',
+											id : 'channel-{value}-status',
 											cssTemplate : 'text-center',
 										},
 										{
 											fieldName : 'activeDate',
 											label : 'Active date',
-											id : 'channel-{value}-active-date-label',
+											id : 'channel-{value}-active-date',
 											filterType : 'date',
 										    format : 'dd/MM/yyyy',
 											cssTemplate : 'text-center',
@@ -77,7 +89,7 @@ angular
 										{
 											fieldName : 'expiryDate',
 										    label : 'Expire date',
-										    id : 'channel-{value}-expire-date-label',
+										    id : 'channel-{value}-expire-date',
 										    filterType : 'date',
 										    format : 'dd/MM/yyyy',
 										    cssTemplate : 'text-center'
@@ -87,7 +99,7 @@ angular
 											sortData : false,
 											cellTemplate : '<scf-button id="channel-{{data.importChannelId}}-setup-button" class="btn-default gec-btn-action" ng-click="ctrl.editChannel(data)" title="Config a channel" ng-hide="!data.completed"><i class="fa fa-cog fa-lg" aria-hidden="true"></i></scf-button>'
 													+ '<scf-button id="channel-{{data.importChannelId}}-warning-setup-button" class="btn-default gec-btn-action" ng-click="ctrl.editChannel(data)" title="Config a channel" ng-hide="data.completed"><img ng-hide="data.completed" data-ng-src="img/gear_warning.png" style="height: 13px; width: 14px;"/></scf-button>'
-													+ '<scf-button class="btn-default gec-btn-action" ng-disabled="true" ng-click="ctrl.deleteChannel(data)" title="Test connection"><i class="glyphicon glyphicon-transfer" aria-hidden="true"></i></scf-button>'
+													+ '<scf-button class="btn-default gec-btn-action" ng-disabled="ctrl.disableTestConnection(data)" ng-click="ctrl.deleteChannel(data)" title="Test connection"><i class="glyphicon glyphicon-transfer" aria-hidden="true"></i></scf-button>'
 										} ]
 							}
 
