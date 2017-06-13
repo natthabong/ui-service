731cbd041f937cee54c93705dc09d245778f66a0
Natrada.kae <Natrada.kae@NBNatrada.gec.co.th>
2017-06-13 15:03:29 +0700

    [FIXED] changed logic for search transaction tracking log.

--- src/main/resources/static/js/app/modules/monitor/transaction-tracking-controller.js
+++ src/main/resources/static/js/app/modules/monitor/transaction-tracking-controller.js
@@ -5,20 +5,38 @@ scfApp.controller('TransactionTrackingController', [ '$scope', 'Service', '$stat
 	function($scope, Service, $stateParams, $log, PagingController, UIFactory, $q, $rootScope, $http, PageNavigation, TransactionTrackingService,ngDialog) {
         var vm = this;
 
-		var trackingCriteria = {
-			
-		}
-
-		vm.pagingController = PagingController.create('api/v1/transaction-trackings/search', trackingCriteria, 'GET');
-
-		vm.viewMessage = function(data){
-
-		}
-
-		vm.searchTrackingLog = function(pageModel){
-			vm.pagingController.search(pageModel);
-		}
-
+        vm.splitePageTxt = '';
+        
+        vm.dateFormat = "dd/MM/yyyy";
+		vm.openDateFrom = false;
+		vm.openDateTo = false;
+		vm.defaultPageSize = '20';
+		vm.defaultPage = 0;
+		
+		vm.logTimeFromHour = '';
+		vm.logTimeFromMinute = '';
+		vm.logTimeToHour = '';
+		vm.logTimeToMinute = '';
+		
+		vm.pageModel = {
+			pageSizeSelectModel : vm.defaultPageSize,
+			totalRecord : 0,
+			totalPage : 0,
+			currentPage : vm.defaultPage,
+			clearSortOrder : false
+		};
+		
+		vm.pageSizeList = [ {
+			label : '10',
+			value : '10'
+		}, {
+			label : '20',
+			value : '20'
+		}, {
+			label : '50',
+			value : '50'
+		} ];
+		
 		vm.dataTable = {
 			identityField: '$rowNo',
 			columns : [
@@ -71,10 +89,185 @@ scfApp.controller('TransactionTrackingController', [ '$scope', 'Service', '$stat
 					cellTemplate : '<scf-button ng-disabled="data.trackingDetail" class="btn-default gec-btn-action" id="view-{{data.$rowNo}}-view-button" ng-click="ctrl.viewMessage(data)" title="View"><i class="fa fa-search" aria-hidden="true"></i></scf-button>'
 				} ]
 		}
+		
+		vm.searchCriteria = {
+			logDateFrom : undefined,
+			logDateTo : undefined,
+			refNo : '',
+			processNo : ''
+		}
+		
+		vm.initLoad = function() {
+			vm.searchTrackingLog();
+		}
+		
+		function prepareCriteria() {
+			if (angular.isDate(vm.logListModel.logDateFrom)) {
+				vm.searchCriteria.logDateFrom = vm.logListModel.logDateFrom
 
-		var initial = function(){
+				var hour = 0;
+				var min = 0;
+				
+				if(vm.logTimeFromHour!==''&&vm.logTimeFromMinute!==''){
+					hour = vm.logTimeFromHour;
+					min = vm.logTimeFromMinute;
+				}		
+				
+				var datetime = new Date(vm.logListModel.logDateFrom.getFullYear(), 
+						vm.logListModel.logDateFrom.getMonth(), 
+						vm.logListModel.logDateFrom.getDate(), 
+						hour, min, 0);
+				vm.searchCriteria.logDateFrom = datetime;
+			}else{
+				vm.searchCriteria.logDateFrom = undefined;
+			}
+
+			if (angular.isDate(vm.logListModel.logDateTo)) {
+				vm.searchCriteria.logDateTo = vm.logListModel.logDateTo;
+				
+				var hour = 23;
+				var min = 59;
+				
+				if(vm.logTimeToHour!==''&&vm.logTimeToMinute!==''){
+					hour = vm.logTimeToHour;
+					min = vm.logTimeToMinute;
+				}		
+				
+				var datetime = new Date(vm.logListModel.logDateTo.getFullYear(), 
+						vm.logListModel.logDateTo.getMonth(), 
+						vm.logListModel.logDateTo.getDate(), 
+						hour, min, 0);
+				vm.searchCriteria.logDateTo = datetime;
+			}else{
+				vm.searchCriteria.logDateTo = undefined;
+			}
+			
+			vm.searchCriteria.refNo = UIFactory.createCriteria(vm.logListModel.refNo);
+			vm.searchCriteria.processNo = UIFactory.createCriteria(vm.logListModel.processNo);
+			
+			return vm.searchCriteria;
 
 		}
 
-		initial();
+		vm.pagingController = PagingController.create('api/v1/transaction-trackings', vm.searchCriteria, 'GET');
+
+		vm.viewMessage = function(data){
+
+		}
+
+		vm.searchTrackingLog = function(pageModel){
+			if (isValid()) {
+				var criteria = prepareCriteria();
+				var logDiferred = vm.pagingController.search(pageModel);
+				vm.showInfomation = true;
+			}
+		}
+		
+		vm.openCalendarDateFrom = function() {
+			vm.openDateFrom = true;
+		}
+
+		vm.openCalendarDateTo = function() {
+			vm.openDateTo = true;
+		}
+		
+		var isValid = function() {
+			var valid = true;
+			vm.wrongDateFormat = false;
+			vm.wrongDateFromTo = false;
+			
+			//Wrong date format
+			if (angular.isUndefined(vm.logListModel.logDateFrom)||angular.isUndefined(vm.logListModel.logDateTo)) {
+				valid = false;
+			}else{
+				if(vm.logTimeFromHour!==''&&vm.logTimeFromMinute!==''&&!angular.isDate(vm.logListModel.logDateFrom)){
+					valid = false;
+				}else if(vm.logTimeToHour!==''&&vm.logTimeToMinute!==''&&!angular.isDate(vm.logListModel.logDateTo)){
+					valid = false;
+				}
+			}
+			
+			//Wrong time format
+			if(angular.isDefined(vm.logListModel.logDateFrom)){
+				if((vm.logTimeFromHour!==''&&vm.logTimeFromMinute!=='')||(vm.logTimeFromHour===''&&vm.logTimeFromMinute==='')){
+					if(vm.logTimeFromHour.length == 1 || vm.logTimeFromHour<0 || vm.logTimeFromHour>=24){
+						valid = false;
+					}else{
+						if (vm.logTimeFromMinute.length == 1 || vm.logTimeFromMinute<0 || vm.logTimeFromMinute>=60) {
+							valid = false;
+						}
+					}
+				}else{
+					valid = false;
+				}
+			}
+			
+			if(angular.isDefined(vm.logListModel.logDateTo)){
+				if((vm.logTimeToHour!==''&&vm.logTimeToMinute!=='')||(vm.logTimeToHour===''&&vm.logTimeToMinute==='')){
+					if(vm.logTimeToHour.length == 1 || vm.logTimeToHour<0 || vm.logTimeToHour>=24){
+						valid = false;
+					}else{
+						if (vm.logTimeToMinute.length == 1 || vm.logTimeToMinute<0 || vm.logTimeToMinute>=60) {
+							valid = false;
+						}
+					}
+				}else{
+					valid = false;
+				}
+			}
+			
+			if(!valid){
+				vm.wrongDateFormat = true;
+				vm.wrongDateFromTo = false;
+			}else{
+				//Wrong date from to
+				if (angular.isDate(vm.logListModel.logDateFrom)&&angular.isDate(vm.logListModel.logDateTo)) {
+					var hourFrom = vm.logTimeFromHour;
+					var minuteFrom = vm.logTimeFromMinute;
+					var hourTo = vm.logTimeToHour;
+					var minuteTo = vm.logTimeToMinute;
+					
+					if(hourFrom===''){
+						hourFrom = 0;
+					}
+					if(minuteFrom===''){
+						minuteFrom = 0;
+					}
+					if(hourTo===''){
+						hourTo = 23;
+					}
+					if(minuteTo===''){
+						minuteTo = 59;
+					}
+					
+					var datetimeFrom = new Date(vm.logListModel.logDateFrom.getFullYear(), 
+							vm.logListModel.logDateFrom.getMonth(), 
+							vm.logListModel.logDateFrom.getDate(), 
+							hourFrom, minuteFrom, 0);
+					
+					var datetimeTo = new Date(vm.logListModel.logDateTo.getFullYear(), 
+							vm.logListModel.logDateTo.getMonth(), 
+							vm.logListModel.logDateTo.getDate(), 
+							hourTo, minuteTo, 0);
+					
+					if(datetimeFrom > datetimeTo){
+						valid = false;
+						vm.wrongDateFormat = false;
+						vm.wrongDateFromTo = true;
+					}
+				}
+			}
+			
+			return valid;
+		};
+		
+		vm.logListModel = {
+			logDateFrom : '',
+			logDateTo : '',
+			refNo : undefined,
+			processNo : undefined
+		}
+		
+		vm.initLoad();
+		
 } ]);
