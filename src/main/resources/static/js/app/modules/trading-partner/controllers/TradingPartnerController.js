5384d1bf29dcd4d9e6af2186d09b276e15b20bc5
GEC\cheerakwan.sut <cheerakwan@gmail.com>
2017-06-19 15:16:12 +0700

    [NEW] ui new trading partner

--- src/main/resources/static/js/app/modules/trading-partner/controllers/TradingPartnerController.js
+++ src/main/resources/static/js/app/modules/trading-partner/controllers/TradingPartnerController.js
@@ -11,14 +11,14 @@ tpModule.controller('TradingPartnerController', [
 				PagingController, TradingPartnerService){
 
             var vm = this;
-            $scope.tradingPartner = {};
+            vm.tradingPartner = {};
             var mode = {
                 NEW : 'newTradingPartner',
 		    	EDIT : 'editTradingPartner'
             }
             var currentMode = $stateParams.mode;
 
-            vm.initialPage = function(){
+            vm.loadTradingPartner = function(){
                 if(currentMode == mode.EDIT){
 
                 }else{
@@ -26,6 +26,56 @@ tpModule.controller('TradingPartnerController', [
                     vm.isEditMode = false;
                 }
             }
+
+            $scope.cancel = function() {
+                PageNavigation.gotoPreviousPage();
+            }
+
+            $scope.save = function() {
+            
+            }
+
+            var _organizeBuyer = function(query) {
+                var deffered = TradingPartnerService.getOrganizeByNameOrCodeLike(query);
+                deffered.promise.then(function(response) {
+                    response.data.map(function(item) {
+                        item.identity = [ 'buyer-', item.organizeId, '-option' ].join('');
+                        item.label = [ item.organizeId, ': ', item.organizeName ].join('');
+                        return item;
+                    });
+                });
+                return deffered;
+            }
+
+            var _organizeSupplier = function(query) {
+                var deffered = TradingPartnerService.getOrganizeByNameOrCodeLike(query);
+                deffered.promise.then(function(response) {
+                    response.data.map(function(item) {
+                        item.identity = [ 'supplier-', item.organizeId, '-option' ].join('');
+                        item.label = [ item.organizeId, ': ', item.organizeName ].join('');
+                        return item;
+                    });
+                });
+                return deffered;
+            }
+            
+            vm.buyerAutoSuggestModel = UIFactory
+                .createAutoSuggestModel({
+                    placeholder : 'Please enter orgainze name or code',
+                    itemTemplateUrl : 'ui/template/autoSuggestTemplate.html',
+                    query : _organizeBuyer
+                });
+
+            vm.supplierAutoSuggestModel = UIFactory
+                .createAutoSuggestModel({
+                    placeholder : 'Please enter orgainze name or code',
+                    itemTemplateUrl : 'ui/template/autoSuggestTemplate.html',
+                    query : _organizeSupplier
+                });
+
+            var init = function() {
+			    vm.loadTradingPartner();
+			}();
 
         }
 ]);
