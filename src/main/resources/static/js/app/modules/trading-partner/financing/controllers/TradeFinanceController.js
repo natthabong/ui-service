5415fb00f8daf7b054df363bd11c6bf9ace7510c
GEC\tapanapong.kha <tapanapong.kha@gec.co.th>
2017-06-21 17:07:46 +0700

    [FIXED] update finance ui

--- src/main/resources/static/js/app/modules/trading-partner/financing/controllers/TradeFinanceController.js
+++ src/main/resources/static/js/app/modules/trading-partner/financing/controllers/TradeFinanceController.js
@@ -1,7 +1,8 @@
 'use strict';
 var tradeFinanceModule = angular.module('scfApp');
-tradeFinanceModule.controller('TradeFinanceController',['$scope','$stateParams','UIFactory','PageNavigation','PagingController','$log',
-    function($scope, $stateParams, UIFactory,PageNavigation, PagingController,$log) {
+tradeFinanceModule.controller('TradeFinanceController',['$scope','$stateParams','UIFactory',
+    'PageNavigation','PagingController','$log','$http','TradeFinanceService',
+    function($scope, $stateParams, UIFactory,PageNavigation, PagingController,$log,$http,TradeFinanceService) {
 
         var vm = this;
         var log = $log;
@@ -30,37 +31,38 @@ tradeFinanceModule.controller('TradeFinanceController',['$scope','$stateParams',
             }
         ];
 
-        vm.loanAccountAutoSuggestModel = UIFactory.createAutoSuggestModel({
-			placeholder : 'Please enter supplier account no.',
-			itemTemplateUrl : 'ui/template/autoSuggestTemplate.html',
-			query : querySupplierCode
-		});
-
-        var querySupplierCode = function(value) {
-			var sponsorId = vm.documentListModel.sponsor.organizeId;
-			var supplierCodeServiceUrl = 'api/v1/suppliers';
+        var queryAccount = function(value) {
+			var organizeId = vm.tradeFinanceModel.borrower;
+			var accountServiceUrl = 'api/v1/organize-customers/'+organizeId+'/accounts';
 
 			value = value = UIFactory.createCriteria(value);
 			
-			return $http.get(supplierCodeServiceUrl, {
+			return $http.get(accountServiceUrl, {
 				params : {
 					q : value,
-					sponsorId : sponsorId,
 					offset : 0,
 					limit : 5
 				}
 			}).then(function(response) {
+                console.log(response)
 				return response.data.map(function(item) {
-					item.identity = [ 'supplier-', item.organizeId, '-option' ].join('');
-					item.label = [ item.organizeId, ': ', item.organizeName ].join('');
+                    console.log(item)
+					item.identity = [ 'account-', item.accountNo, '-option' ].join('');
+					item.label = [ item.accountNo ].join('');
 					return item;
 				});
 			});
 		};
 
+        vm.financeAccountAutoSuggestModel = UIFactory.createAutoSuggestModel({
+			placeholder : 'Please enter supplier account no.',
+			itemTemplateUrl : 'ui/template/autoSuggestTemplate.html',
+			query : queryAccount
+		});
+        
         vm.tradeFinanceModel = {
             borrower : vm.borrowerModel[0].value,
-            loanAccount : null,
+            financeAccount : null,
             tenor : null,
             percentageLoan: null,
             interestRate : null,
@@ -69,17 +71,105 @@ tradeFinanceModule.controller('TradeFinanceController',['$scope','$stateParams',
             isSuspend: false
         };
 
-        var initialTradeFinance = function(){
-            var tradeFinanceData = $stateParams.data;
-            console.log(tradeFinanceData)
-            vm.tradeFinanceModel.borrower = tradeFinanceData.borrowerId;
-            vm.tradeFinanceModel.loanAccount = tradeFinanceData.accountNo;
-            vm.tradeFinanceModel.tenor = tradeFinanceData.tenor;
-            vm.tradeFinanceModel.percentageLoan = tradeFinanceData.prePercentageDrawdown;
-            vm.tradeFinanceModel.interestRate = tradeFinanceData.interestRate;
-            vm.tradeFinanceModel.agreementDate = null;
-            vm.tradeFinanceModel.creditExpirationDate = null;
-            vm.tradeFinanceModel.isSuspend = false;
+        var prepareAutoSuggestLabel = function(account) {
+            var item = {
+                accountNo : account,
+                identity : [ 'account-', account, '-option' ].join(''),
+                label : [ account ].join(''),
+            }
+			return item;
+		}
+
+        var initialTradeFinance = function(data){
+            var tradeFinanceData = data;
+            if(tradeFinanceData != null){
+                vm.tradeFinanceModel.borrower = tradeFinanceData.borrowerId;
+                vm.tradeFinanceModel.financeAccount = prepareAutoSuggestLabel(tradeFinanceData.accountNo);
+                vm.tradeFinanceModel.tenor = tradeFinanceData.tenor;
+                vm.tradeFinanceModel.percentageLoan = tradeFinanceData.prePercentageDrawdown;
+                vm.tradeFinanceModel.interestRate = tradeFinanceData.interest_rate;
+                vm.tradeFinanceModel.agreementDate = new Date(tradeFinanceData.agreementDate);
+                vm.tradeFinanceModel.creditExpirationDate = new Date(tradeFinanceData.limitExpiryDate);
+                vm.tradeFinanceModel.isSuspend = tradeFinanceData.suspend;
+            }
+        }
+
+        var getTradeFinanceInfo = function(sponsorId,supplierId,accountId){
+			var defered = TradeFinanceService.getTradeFinanceInfo(sponsorId,supplierId,accountId);
+			defered.promise.then(function(response) {
+                initialTradeFinance(response.data)
+			}).catch(function(response) {
+				log.error('Get trading finance fail');
+			});
+		}
+
+        var _validate = function(){
+            $scope.errors = {};
+            var valid = true;
+            console.log(vm.tradeFinanceModel)
+
+            if(vm.tradeFinanceModel.financeAccount != null && angular.isObject(vm.tradeFinanceModel.financeAccount)){
+                
+            }else{
+                valid = false;
+                $scope.errors.financeAccount = {
+                    message : 'Finance account is required.'
+                }
+            }
+
+            if(vm.tradeFinanceModel.tenor != null){
+                // vm.tradingPartner.sponsorId = vm.organizeListModel.buyer.organizeId;
+            }else{
+                valid = false;
+                $scope.errors.tenor = {
+                    message : 'Tenor (Day) is required.'
+                }
+            }
+
+            if(vm.tradeFinanceModel.percentageLoan != null){
+                // vm.tradingPartner.sponsorId = vm.organizeListModel.buyer.organizeId;
+            }else{
+                valid = false;
+                $scope.errors.percentageLoan = {
+                    message : 'Percentage loan (%) is required.'
+                }
+            }
+
+            if(vm.tradeFinanceModel.agreementDate != null){
+                if(angular.isDate(vm.tradeFinanceModel.agreementDate)){
+                
+                }else{
+                    valid = false;
+                    $scope.errors.agreementDate = {
+                        message : 'Wrong date format data.'
+                    }
+                }
+            }else{
+                valid = false;
+                $scope.errors.agreementDate = {
+                    message : 'Agreement date is required.'
+                }
+            }
+
+            if(vm.tradeFinanceModel.creditExpirationDate != null){
+                if(angular.isDate(vm.tradeFinanceModel.creditExpirationDate)){
+                
+                }else{
+                    valid = false;
+                    $scope.errors.creditExpirationDate = {
+                        message : 'Wrong date format data.'
+                    }
+                }
+            }else{
+                valid = false;
+                $scope.errors.creditExpirationDate = {
+                    message : 'Credit expiration date is required.'
+                }
+            }
+
+            console.log($scope.error)
+            
+            return valid;
         }
 
         var initLoad = function() {
@@ -90,8 +180,12 @@ tradeFinanceModule.controller('TradeFinanceController',['$scope','$stateParams',
                 if($stateParams.data == ''){
                     log.error("Trade finance data is null.")
                     PageNavigation.gotoPage('/trading-partners');
+                }else{
+                    var sponsorId = $stateParams.data.sponsorId;
+                    var supplierId = $stateParams.data.supplierId;
+                    var accountId = $stateParams.data.accountId;
+                    getTradeFinanceInfo(sponsorId,supplierId,accountId);
                 }
-                initialTradeFinance();
             }
         }();
 
@@ -116,7 +210,10 @@ tradeFinanceModule.controller('TradeFinanceController',['$scope','$stateParams',
         }
 		
         vm.save = function(){
-            
+            console.log("Hi")
+            if(_validate()){
+
+            }
         }
 
         vm.cancel = function(){
--- src/main/resources/static/js/app/modules/trading-partner/financing/services/TradeFinanceService.js
+++ src/main/resources/static/js/app/modules/trading-partner/financing/services/TradeFinanceService.js
@@ -1,7 +1,13 @@
 'use strict';
 var tradeFinanceModule = angular.module('scfApp');
-tradeFinanceModule.factory('TradeFinanceService', [ '$http', '$q', function($http, $q) {
+tradeFinanceModule.factory('TradeFinanceService', [ '$http', '$q','Service', function($http, $q, Service ) {
+
+	function getTradeFinanceInfo(sponsorId,supplierId,accountId){
+		var url = '/api/v1/organize-customers/'+sponsorId+'/trading-partners/'+supplierId+'/trade-finance/'+accountId;
+		return Service.doGet(url);
+	}
 
 	return {
+		getTradeFinanceInfo:getTradeFinanceInfo
 	}
 } ]);
