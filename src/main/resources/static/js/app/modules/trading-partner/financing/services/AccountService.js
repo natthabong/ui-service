81b915990f9fdd9f879e4c7f6a8569d51e0536c9
GEC\natthapong.noo <natthapong.noo@gec.co.th>
2017-06-22 18:37:43 +0700

    [FIXED] Error message when account is existed.

--- src/main/resources/static/js/app/modules/trading-partner/financing/services/AccountService.js
+++ src/main/resources/static/js/app/modules/trading-partner/financing/services/AccountService.js
@@ -3,8 +3,19 @@ var app = angular.module('gecscf.tradingPartner.financing');
 app.factory('AccountService', [ '$http', '$q','Service', function($http, $q, Service ) {
 
 	var save = function(account){
-		var url = '/api/v1/organize-customers/'+account.organizeId+'/accounts';
-		return Service.doPost(url, account);
+		var serviceUrl = '/api/v1/organize-customers/'+account.organizeId+'/accounts';
+		var deferred = $q.defer();
+		$http({
+			method : 'POST',
+			url : serviceUrl,
+			data: account
+		}).then(function(response) {
+			return deferred.resolve(response);
+		}).catch(function(response) {
+			return deferred.reject(response);
+		});
+		return deferred;
+	
 	}
 
 	return {
