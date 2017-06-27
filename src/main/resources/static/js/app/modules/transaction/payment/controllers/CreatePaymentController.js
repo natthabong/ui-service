924389b83e9739e088994c899d14f4f565aa3df3
GEC\tapanapong.kha <tapanapong.kha@gec.co.th>
2017-06-27 15:21:28 +0700

    [FIXED] edit pagging controller

--- src/main/resources/static/js/app/modules/transaction/payment/controllers/CreatePaymentController.js
+++ src/main/resources/static/js/app/modules/transaction/payment/controllers/CreatePaymentController.js
@@ -1,93 +1,20 @@
 var txnMod = angular.module('gecscf.transaction');
 txnMod.controller('CreatePaymentController', ['$rootScope', '$scope', 'SCFCommonService', 'CreatePaymentService',
-		'PagingController', function($rootScope, $scope, SCFCommonService, CreatePaymentService, PagingController) {
+		'PagingController','$log', function($rootScope, $scope, SCFCommonService, CreatePaymentService, PagingController,$log) {
 	
 	var vm = this;
+	var log = $log;
 
-	var sponsorId = $rootScope.userInfo.organizeId;
-	console.log(sponsorId)
+	var ownerId = $rootScope.userInfo.organizeId;
 	
+	vm.criteria = {
+
+	}
+
 	var pageOptions = {
 			
 	}
-
-	vm.pageSizeList = [{
-            label: '10',
-            value: '10'
-        }, {
-            label: '20',
-            value: '20'
-        }, {
-            label: '50',
-            value: '50'
-        }];
-
-	vm.pageModel = {
-		pageSizeSelectModel: '20',
-		totalRecord: 0,
-		currentPage: 0,
-		clearSortOrder: false
-	};
-
-	var _loadDocument = function() {
-		var loadDocumentDiferred = $q.defer();
-		var deffered = CreatePaymentService.getDocument(criteria);
-		deffered.promise
-			.then(function(response) {
-				// response success
-				vm.pageModel.totalRecord = response.headers('X-Total-Count');
-				vm.pageModel.totalPage = response.headers('X-Total-Page');
-				// Generate Document for display
-				vm.tableRowCollection = response.data;
-				
-				// Calculate Display page
-				vm.splitePageTxt = SCFCommonService.splitePage(vm.pageModel.pageSizeSelectModel, vm.pageModel.currentPage, vm.pageModel.totalRecord);
-				loadDocumentDiferred.resolve('Success');
-			})
-			.catch(function(response) {
-				log.error(response);
-				loadDocumentDiferred.reject('Fail');
-			});
-		return loadDocumentDiferred;
-	}
-
-	vm.searchDocument = function(pagingModel) {
-		var searchDocumentDeferred = $q.defer();
-
-		if (pagingModel === undefined) {
-			vm.pageModel.clearSortOrder = !vm.pageModel.clearSortOrder;
-			vm.createTransactionModel.order = '';
-			vm.createTransactionModel.orderBy = '';
-			vm.pageModel.pageSizeSelectModel = '20';
-			vm.pageModel.currentPage = 0;
-			_loadDocument();
-		} else {
-			vm.pageModel.pageSizeSelectModel = pagingModel.pageSize;
-			vm.pageModel.currentPage = pagingModel.page;
-			_loadDocument();
-		}
-		return searchDocumentDeferred;
-	};
-
-
-	vm.dataTable = {
-		options: {
-			displaySelect: {
-				label: '<input type="checkbox" id="select-all-checkbox" ng-model="ctrl.checkAllModel" ng-click="ctrl.checkAllDocument()"/>',
-				cssTemplate: 'text-center',
-				cellTemplate: '<input type="checkbox" checklist-model="ctrl.documentSelects" checklist-value="data" ng-click="ctrl.selectDocument(data)"/>',
-				displayPosition: 'first',
-				idValueField: '$rowNo',
-				id: 'document-{value}-checkbox'
-			}
-		},
-		columns: []
-	};
 	
-
-	vm.criteria = {
-			
-	}
 	vm.documentSelects = []
 	
 	vm.dataTable = {
@@ -157,13 +84,14 @@ txnMod.controller('CreatePaymentController', ['$rootScope', '$scope', 'SCFCommon
             log.error(response);
         });
     };
+
 	function _prepareCriteria() {
 		
 		return {
 			
 		};
 	}
-	
+
 	vm.pagingController = PagingController.create('api/v1/documents', vm.criteria, 'GET');
 	
 	vm.searchDocument = function(pagingModel) {
