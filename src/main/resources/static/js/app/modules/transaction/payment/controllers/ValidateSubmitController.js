a19a865d64945280ea6ca1e6cc44074e32c94e73
GEC\cheerakwan.sut <cheerakwan@gmail.com>
2017-06-28 15:02:30 +0700

    [FIXED] ui validate and submit payment

--- src/main/resources/static/js/app/modules/transaction/payment/controllers/ValidateSubmitController.js
+++ src/main/resources/static/js/app/modules/transaction/payment/controllers/ValidateSubmitController.js
@@ -17,6 +17,15 @@ paymentModule.controller('ValidateSubmitController', [
         vm.initLoadData = function() {
             vm.transactionModel = $stateParams.transactionModel;
             vm.tradingpartnerInfoModel = $stateParams.tradingpartnerInfoModel;
+
+            if(vm.transactionModel.payerAccount != null && vm.transactionModel.payerAccount != ''){
+                var payerAccount = vm.transactionModel.payerAccount;
+                var word1 = payerAccount.substring(0,3);
+                var word2 = payerAccount.substring(3,4);
+                var word3 = payerAccount.substring(4, 9);
+                var word4 = payerAccount.substring(9,10);
+                vm.transactionModel.payerAccount = word1+'-'+word2+'-'+word3+'-'+word4;
+            }
         }
 
         vm.pagingController = PagingController.create(vm.transactionModel.documents);
