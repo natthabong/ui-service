299848dc5f252b7c5b5e3212555755cdbeac175c
GEC\cheerakwan.sut <cheerakwan@gmail.com>
2017-06-28 17:00:51 +0700

    [FIXED] ui validate and submit payment

--- src/main/resources/static/js/app/modules/transaction/payment/controllers/ValidateSubmitController.js
+++ src/main/resources/static/js/app/modules/transaction/payment/controllers/ValidateSubmitController.js
@@ -10,26 +10,36 @@ paymentModule.controller('ValidateSubmitController', [
     'CreatePaymentService',
     function($scope, $stateParams, UIFactory, PageNavigation, 
         PagingController, $timeout, SCFCommonService, CreatePaymentService) {
+		
         var vm = this;
-        vm.transactionModel = {};
-        vm.tradingpartnerInfoModel = {};
+        vm.transactionModel = $stateParams.transactionModel;
+        vm.tradingpartnerInfoModel = $stateParams.tradingpartnerInfoModel;
+        vm.pagingController = PagingController.create(vm.transactionModel.documents);
+        
+        vm.dataTable = {
+            columns: []
+        };
 
-        vm.initLoadData = function() {
-            vm.transactionModel = $stateParams.transactionModel;
-            vm.tradingpartnerInfoModel = $stateParams.tradingpartnerInfoModel;
-
-            if(vm.transactionModel.payerAccount != null && vm.transactionModel.payerAccount != ''){
-                var payerAccount = vm.transactionModel.payerAccount;
-                var word1 = payerAccount.substring(0,3);
-                var word2 = payerAccount.substring(3,4);
-                var word3 = payerAccount.substring(4, 9);
-                var word4 = payerAccount.substring(9,10);
-                vm.transactionModel.payerAccount = word1+'-'+word2+'-'+word3+'-'+word4;
-            }
+        var loadDocumentDisplayConfig = function(ownerId, mode) {
+            var deffered = SCFCommonService.getDocumentDisplayConfig(ownerId, mode);
+            deffered.promise.then(function(response) {
+                vm.dataTable.columns = response.items;
+            });
         }
 
-        vm.pagingController = PagingController.create(vm.transactionModel.documents);
-
+        if(vm.transactionModel.payerAccount != null && vm.transactionModel.payerAccount != ''){
+            var payerAccount = vm.transactionModel.payerAccount;
+            var word1 = payerAccount.substring(0,3);
+            var word2 = payerAccount.substring(3,4);
+            var word3 = payerAccount.substring(4, 9);
+            var word4 = payerAccount.substring(9,10);
+            vm.transactionModel.payerAccount = word1+'-'+word2+'-'+word3+'-'+word4;
+        }
+        
+        vm.searchDocument = function(pagingModel) {
+        	vm.pagingController.search(pagingModel);
+        }
+        
         vm.backToCreate = function(){
             $timeout(function(){
                 PageNavigation.backStep();
@@ -41,7 +51,8 @@ paymentModule.controller('ValidateSubmitController', [
         };
 
         var init = function() {
-            vm.initLoadData();
+            vm.pagingController.search();
+            loadDocumentDisplayConfig(vm.transactionModel.supplierId, 'BFP');
         }();
     }
 ]);
