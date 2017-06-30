a155fb60a6be47f542288087c0f45c494a7d70e2
GEC\cheerakwan.sut <cheerakwan@gmail.com>
2017-06-30 17:20:11 +0700

    [FIXED] ui view

--- src/main/resources/static/js/app/modules/transaction/payment/controllers/ViewPaymentController.js
+++ src/main/resources/static/js/app/modules/transaction/payment/controllers/ViewPaymentController.js
@@ -20,25 +20,30 @@ paymentModule.controller('ViewPaymentController', [
                 columns: []
             };
 
-            var loadTransaction = function(){
+            var loadTransaction = function(callback){
 
                 var deffered = ViewPaymentService.getTransaction(vm.transactionModel);
                 deffered.promise.then(function(response){
+                	vm.transactionModel = response.data; 
                     vm.transactionModel.supplier =  response.data.supplierOrganize.organizeName;
                     vm.transactionModel.sponsor =  response.data.sponsorOrganize.organizeName;
+                    vm.transactionModel.documents = response.data.documents;
                     
                     vm.pagingController = PagingController.create(response.data.documents);
-                    vm.searchDocument();
+                    
+                    loadDocumentDisplayConfig(vm.transactionModel.supplierId, 'BFP', vm.searchDocument);
+                    
                 })
                 .catch(function(response){
                     console.log('view payment load error');
                 })
             }
 
-            var loadDocumentDisplayConfig = function(ownerId, mode) {
+            var loadDocumentDisplayConfig = function(ownerId, mode, callback) {
                 var deffered = SCFCommonService.getDocumentDisplayConfig(ownerId, mode);
                 deffered.promise.then(function(response) {
                     vm.dataTable.columns = response.items;
+                    callback();
                 });
             }
 
@@ -58,7 +63,7 @@ paymentModule.controller('ViewPaymentController', [
 
             var init = function() {
                 loadTransaction();
-                loadDocumentDisplayConfig(vm.transactionModel.supplierId, 'BFP');
+                
             }();
     }
 ]);
