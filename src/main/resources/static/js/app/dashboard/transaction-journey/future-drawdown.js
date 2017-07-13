f4df6ebc834ba4ea9e80f82510a325168405e31a
GEC\Natrada.kae <natrada.kae@gec.co.th>
2017-07-13 17:59:10 +0700

    [FIXED] changed logic for get dashboard of wait for verify, approve, future drawdown.

--- src/main/resources/static/js/app/dashboard/transaction-journey/future-drawdown.js
+++ src/main/resources/static/js/app/dashboard/transaction-journey/future-drawdown.js
@@ -1,9 +1,10 @@
 angular.module('scfApp').controller(
 		'JourneyFutureDrawdownController', 
 		[
-		 	'$scope', 'Service', 'PageNavigation', 'SCFCommonService', 
-		 	function($scope, Service, PageNavigation, SCFCommonService){
+		 	'$scope', 'Service', 'PageNavigation', 'SCFCommonService', '$rootScope', 
+		 	function($scope, Service, PageNavigation, SCFCommonService, $rootScope){
 				var vm = this;
+				var organizeId = $rootScope.userInfo.organizeId;
 				var compositParent = $scope.$parent;
 				var dashboardParent = compositParent.$parent.it;
 				var dashboardItemParent = dashboardParent.dashboardItem;
@@ -16,47 +17,39 @@ angular.module('scfApp').controller(
 				};
 				
 				vm.transactionCriteria = {
-						dateType: '',
-						dateFrom: '',
-						dateTo: '',
-						sponsorId: '',
-						statusGroup: '',
-						statusCode: '',
-						supplierCode: '',
-						page: 0,
-						pageSize: 20,
-						orders: orderItems
+					supplierId: organizeId,
+					statusCode: 'WAIT_FOR_DRAWDOWN_RESULT'
 				}
 				
-			    var orderItems = splitCriteriaSortOrderData(dashboardParent.orderItems);
-			    splitCriteriaFilterData(dashboardParent.filterItems);
-			
-			    function splitCriteriaSortOrderData(data) {
-					var dataSplit = data.split(",");
-					var order = [];
-					dataSplit.forEach(function(orderData) {
-						var orderItem = orderData.split(":");
-						item = {
-							fieldName : orderItem[0],
-							direction : orderItem[1]
-						}
-						order.push(item);
-					});
-
-					return order;
-				}
-			    
-			    function splitCriteriaFilterData(data) {
-					var dataSplit = data.split(",");
-					var count = 0;
-					dataSplit.forEach(function(filterData) {
-						var filterItem = filterData.split(":");
-						vm.transactionCriteria[filterItem[0]] = filterItem[1];
-					});
-				}
+//			    var orderItems = splitCriteriaSortOrderData(dashboardParent.orderItems);
+//			    splitCriteriaFilterData(dashboardParent.filterItems);
+//			
+//			    function splitCriteriaSortOrderData(data) {
+//					var dataSplit = data.split(",");
+//					var order = [];
+//					dataSplit.forEach(function(orderData) {
+//						var orderItem = orderData.split(":");
+//						item = {
+//							fieldName : orderItem[0],
+//							direction : orderItem[1]
+//						}
+//						order.push(item);
+//					});
+//
+//					return order;
+//				}
+//			    
+//			    function splitCriteriaFilterData(data) {
+//					var dataSplit = data.split(",");
+//					var count = 0;
+//					dataSplit.forEach(function(filterData) {
+//						var filterItem = filterData.split(":");
+//						vm.transactionCriteria[filterItem[0]] = filterItem[1];
+//					});
+//				}
 			    
 			    vm.load = function() {
-			    	var newDocumentDeferred = Service.requestURL('/api/summary-transaction/get', vm.transactionCriteria);
+			    	var newDocumentDeferred = Service.requestURL('/api/summary-transaction/get', vm.transactionCriteria, 'GET');
 					newDocumentDeferred.promise.then(function(response){
 						vm.futureDrawdownModel.totalTransaction = response.totalTransaction;
 						vm.futureDrawdownModel.totalAmount = SCFCommonService.shortenLargeNumber(response.totalAmount);
--- src/main/resources/static/js/app/dashboard/transaction-journey/wait-for-approve.js
+++ src/main/resources/static/js/app/dashboard/transaction-journey/wait-for-approve.js
@@ -4,6 +4,7 @@ angular.module('scfApp').controller(
 		 	'$scope', 'Service', 'PageNavigation', '$cookieStore', '$state', 'SCFCommonService', '$rootScope', 
 		 	function($scope, Service, PageNavigation, $cookieStore, $state, SCFCommonService, $rootScope) {
 			    var vm = this;
+			    var organizeId = $rootScope.userInfo.organizeId;
 			    var compositParent = $scope.$parent;
 			    var dashboardParent = compositParent.$parent.it;
 			    var dashboardItemParent = dashboardParent.dashboardItem;
@@ -29,47 +30,39 @@ angular.module('scfApp').controller(
 			    };
 			    
 			    vm.transactionCriteria = {
-						dateType: '',
-						dateFrom: '',
-						dateTo: '',
-						sponsorId: '',
-						statusGroup: '',
-						statusCode: '',
-						supplierCode: '',
-						page: 0,
-						pageSize: 20,
-						orders: orderItems
+			    		supplierId: organizeId,
+						statusCode: 'WAIT_FOR_APPROVE'
 				}
 				
-			    var orderItems = splitCriteriaSortOrderData(dashboardParent.orderItems);
-			    splitCriteriaFilterData(dashboardParent.filterItems);
-			
-			    function splitCriteriaSortOrderData(data) {
-					var dataSplit = data.split(",");
-					var order = [];
-					dataSplit.forEach(function(orderData) {
-						var orderItem = orderData.split(":");
-						item = {
-							fieldName : orderItem[0],
-							direction : orderItem[1]
-						}
-						order.push(item);
-					});
-
-					return order;
-				}
-			    
-			    function splitCriteriaFilterData(data) {
-					var dataSplit = data.split(",");
-					var count = 0;
-					dataSplit.forEach(function(filterData) {
-						var filterItem = filterData.split(":");
-						vm.transactionCriteria[filterItem[0]] = filterItem[1];
-					});
-				}
+//			    var orderItems = splitCriteriaSortOrderData(dashboardParent.orderItems);
+//			    splitCriteriaFilterData(dashboardParent.filterItems);
+//			
+//			    function splitCriteriaSortOrderData(data) {
+//					var dataSplit = data.split(",");
+//					var order = [];
+//					dataSplit.forEach(function(orderData) {
+//						var orderItem = orderData.split(":");
+//						item = {
+//							fieldName : orderItem[0],
+//							direction : orderItem[1]
+//						}
+//						order.push(item);
+//					});
+//
+//					return order;
+//				}
+//			    
+//			    function splitCriteriaFilterData(data) {
+//					var dataSplit = data.split(",");
+//					var count = 0;
+//					dataSplit.forEach(function(filterData) {
+//						var filterItem = filterData.split(":");
+//						vm.transactionCriteria[filterItem[0]] = filterItem[1];
+//					});
+//				}
 			    
 			    vm.load = function() {
-			    	var newDocumentDeferred = Service.requestURL('/api/summary-transaction/get', vm.transactionCriteria);
+			    	var newDocumentDeferred = Service.requestURL('/api/summary-transaction/get', vm.transactionCriteria, 'GET');
 			    	newDocumentDeferred.promise.then(function(response){
 			    		vm.waitForApproveModel.totalTransaction = response.totalTransaction;
 						vm.waitForApproveModel.totalAmount = SCFCommonService.shortenLargeNumber(response.totalAmount);
