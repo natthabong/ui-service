1af1423de3b0940a43a234696117642ee331b120
GEC\kowit.tip <kowitcom@gmail.com>
2017-08-09 10:49:07 +0700

    [FIXED] add account type LOAN in Trade finnace config page

--- src/main/resources/static/js/app/modules/trading-partner/financing/controllers/TradeFinanceController.js
+++ src/main/resources/static/js/app/modules/trading-partner/financing/controllers/TradeFinanceController.js
@@ -47,7 +47,8 @@ tradeFinanceModule.controller('TradeFinanceController',['$scope','$stateParams',
 				params : {
 					q : value,
 					offset : 0,
-					limit : 5
+					limit : 5,
+					accountType : "LOAN"
 				}
 			}).then(function(response) {
 				return response.data.map(function(item) {
