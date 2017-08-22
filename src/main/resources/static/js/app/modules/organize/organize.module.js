9d70b67b872d2421af958a44092151949a224ef2
GEC\natthapong.noo <natthapong.noo@gec.co.th>
2017-08-22 16:30:29 +0700

    [CHANGE] Organize configuration.

---
+++ src/main/resources/static/js/app/modules/organize/organize.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('gecscf.organize', [ 'ui.router', 'gecscf.ui','gecscf.organize.configuration' ]).config(
+		[ '$stateProvider', function($stateProvider) {
+			
+		} ]);
