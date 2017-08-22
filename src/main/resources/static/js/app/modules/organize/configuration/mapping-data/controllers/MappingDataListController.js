61e8b02e7e0419223400ca1daf5b5dbd9222e40b
GEC\natthapong.noo <natthapong.noo@gec.co.th>
2017-08-22 18:14:16 +0700

    [FIXED] Layout on organize configuration page.

---
+++ src/main/resources/static/js/app/modules/organize/configuration/mapping-data/controllers/MappingDataListController.js
@@ -0,0 +1,21 @@
+'use strict';
+var tpModule = angular.module('gecscf.organize.configuration');
+tpModule.controller('MappingDataListController', [
+        '$scope',
+		'$stateParams',
+		'UIFactory',
+		'PageNavigation',
+		'PagingController',
+		'MappingDataService',
+        function($scope, $stateParams, UIFactory, PageNavigation,
+				PagingController, MappingDataService){
+
+            var vm = this;
+            
+            
+            var init = function() {
+            	
+			}();
+
+        }
+]);
