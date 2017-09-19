402ee1ccb379c5f260d97ef0f7072fae73b24b9c
GEC\tapanapong.kha <tapanapong.kha@gec.co.th>
2017-09-19 18:16:42 +0700

    [CHANGE] refector get layout config

--- src/main/resources/static/js/app/modules/organize/configuration/file-layout/services/FileLayoutService.js
+++ src/main/resources/static/js/app/modules/organize/configuration/file-layout/services/FileLayoutService.js
@@ -1,218 +1,180 @@
 'use strict';
 var module = angular.module('gecscf.organize.configuration.fileLayout');
-module.factory('FileLayoutService', [ '$http', '$q', function($http, $q) {
-	
-	var getDocumentFields = function(fileLayoutType, sectionType, dataType){
+module.factory('FileLayoutService', ['$http', '$q', 'Service', function ($http, $q, Service) {
+
+	var getDocumentFields = function (fileLayoutType, sectionType, dataType) {
 		var deffered = $q.defer();
-		
-		var uri = 'api/v1/configs/file-layout-types/'+fileLayoutType+'/section-types/'+ sectionType + '/document-field';
+
+		var uri = 'api/v1/configs/file-layout-types/' + fileLayoutType + '/section-types/' + sectionType + '/document-field';
 		$http({
-    	    url : uri,
-        	method: 'GET',
-        	params: {
-        		dataType: dataType
-	    	}
-        		
-	    }).then(function(response) {
-            deffered.resolve(response);
-        }).catch(function(response) {
-           deffered.reject(response);
-        });
-        return deffered;
+			url: uri,
+			method: 'GET',
+			params: {
+				dataType: dataType
+			}
+
+		}).then(function (response) {
+			deffered.resolve(response);
+		}).catch(function (response) {
+			deffered.reject(response);
+		});
+		return deffered;
 	}
 
-//	var getDataTypes = function(dataType){
-//		var deffered = $q.defer();
-//		
-//		var uri = 'api/v1/configs/gecscf/layouts/document-field';
-//		$http({
-//    	    url : uri,
-//        	method: 'GET',
-//        	params: {
-//        		fileLayoutType: 'IMPORT',
-//        		sectionType : 'DETAIL'
-//        	}
-//	    }).then(function(response) {
-//            deffered.resolve(response);
-//        }).catch(function(response) {
-//           deffered.reject(response);
-//        });
-//        return deffered;
-//	}
-//	
-//	var getDisplayDataTypes = function(dataType){
-//		var deffered = $q.defer();
-//		
-//		var uri = 'api/v1/configs/gecscf/layouts/file/data-types';
-//		$http({
-//    	    url : uri,
-//        	method: 'GET',
-//        	params: {
-//        		dataType : dataType,
-//        		isTransient : 'false'
-//        	}
-//	    }).then(function(response) {
-//            deffered.resolve(response);
-//        }).catch(function(response) {
-//           deffered.reject(response);
-//        });
-//        return deffered;
-//	}
-//	
-//	var getValidationDataTypes = function(){
-//		var deffered = $q.defer();
-//		
-//		var uri = 'api/v1/configs/gecscf/layouts/document-field';
-//		$http({
-//    	    url : uri,
-//        	method: 'GET',
-//        	params: {
-//        		fileLayoutType: 'IMPORT',
-//        		sectionType : 'HEADER'
-//        	}
-//	    }).then(function(response) {
-//            deffered.resolve(response);
-//        }).catch(function(response) {
-//           deffered.reject(response);
-//        });
-//        return deffered;
-//	}
-	
-	var getSpecificData = function(){
+	var getFileLayouts = function (owner, processTypes, integrateTypes) {
+		var url = '/api/v1/organize-customers/' + owner + '/process-types/' + processTypes +
+			'/integrate-types/' + integrateTypes + '/layouts';
+		var serviceDiferred = Service.doGet(url, {
+			offset: 0,
+			limit: 999
+		});
+		return serviceDiferred
+	}
+
+	var getFileLayout = function (owner, processTypes, integrateTypes, layoutId) {
+		var url = '/api/v1/organize-customers/' + owner + '/process-types/' + processTypes +
+			'/integrate-types/' + integrateTypes + '/layouts/' + layoutId;
+		var serviceDiferred = Service.doGet(url);
+		return serviceDiferred
+	}
+
+	var getSpecificData = function () {
 
 		var deffered = $q.defer();
 		var uri = 'js/app/modules/organize/configuration/file-layout/data/specifics_data.json';
-		
+
 		$http({
-    	    url : uri,
-        	method: 'GET'
-	    }).then(function(response) {
-	    	deffered.resolve(response);
-		}).catch(function(response) {
+			url: uri,
+			method: 'GET'
+		}).then(function (response) {
+			deffered.resolve(response);
+		}).catch(function (response) {
 			deffered.reject(response);
 		});
-		
+
 		return deffered;
-	
+
 	}
 
-	var loadDataMappingToDropDown = function(sectionType){
+	var loadDataMappingToDropDown = function (sectionType) {
 		var deffered = $q.defer();
-		var uri = 'api/v1/configs/file-layout-types/IMPORT/section-types/'+sectionType+'/document-field'
+		var uri = 'api/v1/configs/file-layout-types/IMPORT/section-types/' + sectionType + '/document-field'
 		$http({
-    	    url : uri,
-        	method: 'GET',
-			params:{
-				isTransient : false
+			url: uri,
+			method: 'GET',
+			params: {
+				isTransient: false
 			}
-	    }).then(function(response) {
-	    	deffered.resolve(response);
-		}).catch(function(response) {
+		}).then(function (response) {
+			deffered.resolve(response);
+		}).catch(function (response) {
 			deffered.reject(response);
 		});
-		
+
 		return deffered;
 	}
-	
-	var validate = function(layout, docFieldList, messageFunc){
-		
+
+	var validate = function (layout, docFieldList, messageFunc) {
+
 		var documentFieldIdList = [];
-		
+
 		console.log(layout);
 		console.log(docFieldList);
-		
-		if(layout.processType=='AR_DOCUMENT'){
-			var errors = { 
-					requireDocDueDate : true,
-					requireNetAmount : true,
-					documentFieldIdListDupplicate : []
-			}	
-			
-			layout.items.forEach(function(item) {
-				if(item.recordType=='DETAIL'){				
+
+		if (layout.processType == 'AR_DOCUMENT') {
+			var errors = {
+				requireDocDueDate: true,
+				requireNetAmount: true,
+				documentFieldIdListDupplicate: []
+			}
+
+			layout.items.forEach(function (item) {
+				if (item.recordType == 'DETAIL') {
 					var docFieldData = docFieldList[item.documentFieldId];
-					if(documentFieldIdList.length > 0 && documentFieldIdList.indexOf(item.documentFieldId)>-1){
-						if(errors.documentFieldIdListDupplicate.indexOf(item.documentFieldId) < 0){
+					if (documentFieldIdList.length > 0 && documentFieldIdList.indexOf(item.documentFieldId) > -1) {
+						if (errors.documentFieldIdListDupplicate.indexOf(item.documentFieldId) < 0) {
 							errors.documentFieldIdListDupplicate.push(item.documentFieldId);
-						}						
-					}else if(item.documentFieldId != null && docFieldData.documentFieldName!=null){
-						if(item.documentFieldId == 14){
+						}
+					} else if (item.documentFieldId != null && docFieldData.documentFieldName != null) {
+						if (item.documentFieldId == 14) {
 							errors.requireNetAmount = false;
-						}else if(item.documentFieldId == 9){
+						} else if (item.documentFieldId == 9) {
 							errors.requireDocDueDate = false;
 						}
 						documentFieldIdList.push(item.documentFieldId);
 					}
-					
-					if(item.valueCloningFields!=null && item.valueCloningFields.length>0){
-						item.valueCloningFields.forEach(function(itemClone) {
-							if(documentFieldIdList.length > 0 && documentFieldIdList.indexOf(itemClone.documentFieldId)>-1){
-								if(errors.documentFieldIdListDupplicate.indexOf(itemClone.documentFieldId) < 0){
+
+					if (item.valueCloningFields != null && item.valueCloningFields.length > 0) {
+						item.valueCloningFields.forEach(function (itemClone) {
+							if (documentFieldIdList.length > 0 && documentFieldIdList.indexOf(itemClone.documentFieldId) > -1) {
+								if (errors.documentFieldIdListDupplicate.indexOf(itemClone.documentFieldId) < 0) {
 									errors.documentFieldIdListDupplicate.push(itemClone.documentFieldId);
 								}
-							}else if(itemClone.documentFieldId != null && docFieldData.documentFieldName!=null){
-								if(itemClone.documentFieldId == 14){
+							} else if (itemClone.documentFieldId != null && docFieldData.documentFieldName != null) {
+								if (itemClone.documentFieldId == 14) {
 									errors.requireNetAmount = false;
-								}else if(itemClone.documentFieldId == 9){
+								} else if (itemClone.documentFieldId == 9) {
 									errors.requireDocDueDate = false;
 								}
 								documentFieldIdList.push(itemClone.documentFieldId);
-							}								
-						});		
-					}	
+							}
+						});
+					}
 				}
 			});
-			
-			if(errors.requireDocDueDate || errors.requireNetAmount || errors.documentFieldIdListDupplicate.length > 0){
+
+			if (errors.requireDocDueDate || errors.requireNetAmount || errors.documentFieldIdListDupplicate.length > 0) {
 				messageFunc(errors);
 				return false;
 			}
-			
-		}else if(layout.processType=='AP_DOCUMENT'){
-			var errors = { 
-				documentFieldIdListDupplicate : []
+
+		} else if (layout.processType == 'AP_DOCUMENT') {
+			var errors = {
+				documentFieldIdListDupplicate: []
 			}
-			
-			layout.items.forEach(function(item) {
-				if(item.recordType=='DETAIL'){
+
+			layout.items.forEach(function (item) {
+				if (item.recordType == 'DETAIL') {
 					var docFieldData = docFieldList[item.documentFieldId];
-					if(documentFieldIdList.length > 0 && documentFieldIdList.indexOf(item.documentFieldId)>-1){
-						if(errors.documentFieldIdListDupplicate.indexOf(item.documentFieldId) < 0){
+					if (documentFieldIdList.length > 0 && documentFieldIdList.indexOf(item.documentFieldId) > -1) {
+						if (errors.documentFieldIdListDupplicate.indexOf(item.documentFieldId) < 0) {
 							errors.documentFieldIdListDupplicate.push(item.documentFieldId);
 						}
-					}else if(item.documentFieldId != null && docFieldData.documentFieldName!=null){
+					} else if (item.documentFieldId != null && docFieldData.documentFieldName != null) {
 
 						documentFieldIdList.push(item.documentFieldId);
 					}
-					
-					if(item.valueCloningFields!=null && item.valueCloningFields.length>0){
-						item.valueCloningFields.forEach(function(itemClone) {
-							if(documentFieldIdList.length > 0 && documentFieldIdList.indexOf(itemClone.documentFieldId)>-1){							
-								if(errors.documentFieldIdListDupplicate.indexOf(itemClone.documentFieldId) < 0){
+
+					if (item.valueCloningFields != null && item.valueCloningFields.length > 0) {
+						item.valueCloningFields.forEach(function (itemClone) {
+							if (documentFieldIdList.length > 0 && documentFieldIdList.indexOf(itemClone.documentFieldId) > -1) {
+								if (errors.documentFieldIdListDupplicate.indexOf(itemClone.documentFieldId) < 0) {
 									errors.documentFieldIdListDupplicate.push(itemClone.documentFieldId);
 								}
-							}else if(itemClone.documentFieldId != null && docFieldData.documentFieldName!=null){
+							} else if (itemClone.documentFieldId != null && docFieldData.documentFieldName != null) {
 
 								documentFieldIdList.push(itemClone.documentFieldId);
-							}								
-						});		
-					}					
+							}
+						});
+					}
 				}
-			});		
-			
-			if(errors.requirePaymentAmount || errors.documentFieldIdListDupplicate.length > 0){
+			});
+
+			if (errors.requirePaymentAmount || errors.documentFieldIdListDupplicate.length > 0) {
 				messageFunc(errors);
 				return false;
 			}
 		}
-		
+
 		messageFunc(errors);
 		return true;
 	}
 	return {
-		loadDataMappingToDropDown:loadDataMappingToDropDown,
+		loadDataMappingToDropDown: loadDataMappingToDropDown,
 		getDocumentFields: getDocumentFields,
 		getSpecificData: getSpecificData,
-		validate: validate
+		validate: validate,
+		getFileLayouts : getFileLayouts,
+		getFileLayout : getFileLayout
 	}
-} ]);
+}]);
