3ae41f314d2c813f3214af56175c9b6c73f5df94
GEC\Natrada.kae <natrada.kae@gec.co.th>
2017-10-18 09:36:39 +0700

    [FIXED] add total section.

--- src/main/resources/static/js/app/modules/transaction/payment/controllers/CreatePaymentWithoutInvoiceController.js
+++ src/main/resources/static/js/app/modules/transaction/payment/controllers/CreatePaymentWithoutInvoiceController.js
@@ -1,7 +1,7 @@
 var txnMod = angular.module('gecscf.transaction');
-txnMod.controller('CreatePaymentWithoutInvoiceController', ['$rootScope', '$scope', '$log', '$stateParams', 'SCFCommonService', 'TransactionService',
-    'PagingController', 'PageNavigation', '$filter', 'CreatePaymentService',
-    function($rootScope, $scope, $log, $stateParams, SCFCommonService, TransactionService, PagingController, PageNavigation, $filter, CreatePaymentService) {
+txnMod.controller('CreatePaymentWithoutInvoiceController', ['$rootScope', '$scope', '$log', '$stateParams', 'SCFCommonService', 'TransactionService', 'CreatePaymentService',
+    'PagingController', 'PageNavigation', '$filter',
+    function($rootScope, $scope, $log, $stateParams, SCFCommonService, TransactionService, CreatePaymentService, PagingController, PageNavigation, $filter) {
 
         var vm = this;
         var _criteria = {};
@@ -39,6 +39,12 @@ txnMod.controller('CreatePaymentWithoutInvoiceController', ['$rootScope', '$scop
             transactionDate: null,
             maturityDate: null
         }
+        
+        vm.tradingpartnerInfoModel = $stateParams.tradingpartnerInfoModel || {
+            available: '0.00',
+            tenor: null,
+            interestRate: null
+        }
 
         var _checkCreatePaymentType = function(supplierId) {
             var result = $.grep(_suppliers, function(td) { return td.supplierId == supplierId; });
@@ -54,6 +60,8 @@ txnMod.controller('CreatePaymentWithoutInvoiceController', ['$rootScope', '$scop
                     }
                 }
                 PageNavigation.gotoPage('/my-organize/create-payment', params);
+            }else {
+                _loadAccount(ownerId, supplierId);
             }
         }
 
@@ -79,8 +87,113 @@ txnMod.controller('CreatePaymentWithoutInvoiceController', ['$rootScope', '$scop
             vm.transactionModel.transactionAmount = CreatePaymentService.calculateTransactionAmount(documentSelects);
         }
 
-        var init = function() {
+        function _loadMaturityDate() {
+            vm.maturityDateDropDown = [];
+            if (angular.isDefined(vm.paymentModel) && vm.transactionModel.documents != [] && vm.transactionModel.documents.length != 0) {
 
+                var deffered = TransactionService.getAvailableMaturityDates(vm.paymentModel, vm.tradingpartnerInfoModel.tenor);
+                deffered.promise.then(function(response) {
+                        var maturityDates = response.data;
+
+                        maturityDates.forEach(function(data) {
+                            vm.maturityDateDropDown.push({
+                                label: data,
+                                value: data
+                            });
+                        });
+                        if (vm.maturityDateDropDown.length != 0) {
+                            vm.maturityDateModel = vm.maturityDateDropDown[0].value;
+                        }
+
+                        if ($stateParams.backAction && vm.transactionModel.maturityDate != null) {
+                            vm.maturityDateModel = vm.transactionModel.maturityDate;
+                        }
+
+                    })
+                    .catch(function(response) {
+                        log.error(response);
+                    });
+            }
+        }
+
+        function _loadPaymentDate() {
+            vm.paymentDropDown = [];
+            vm.transactionModel.documents = vm.documents;
+            console.log(vm.transactionModel.documents);
+            vm.transactionModel.supplierId = vm.criteria.supplierId;
+            if (vm.transactionModel.documents != [] && vm.transactionModel.documents.length != 0) {
+                var deffered = TransactionService.getPaymentDate(vm.transactionModel);
+                deffered.promise.then(function(response) {
+                        var paymentDates = response.data;
+
+                        paymentDates.forEach(function(data) {
+                            vm.paymentDropDown.push({
+                                label: data,
+                                value: data
+                            })
+                        });
+
+                        if ($stateParams.backAction && vm.transactionModel.transactionDate != null) {
+                            vm.paymentModel = vm.transactionModel.transactionDate;
+                        } else {
+                            vm.paymentModel = vm.paymentDropDown[0].value;
+                        }
+                        _loadMaturityDate();
+                    })
+                    .catch(function(response) {
+                        log.error(response);
+                    });
+            } else {
+                _loadMaturityDate();
+            }
+        }
+
+        function _loadAccount(ownerId, supplierId) {
+            vm.accountDropDown = [];
+            var deffered = TransactionService.getAccounts(ownerId, supplierId);
+            deffered.promise.then(function(response) {
+                var accounts = response.data;
+                vm.isLoanPayment = false;
+
+                var loanAccountIndex = 0;
+                accounts.forEach(function(account, index) {
+                    var a = {
+                        label: ($filter('accountNoDisplay')(account.accountNo)),
+                        value: account.accountId,
+                        item: account
+                    };
+                    if (account.accountType == 'LOAN') {
+                        loanAccountIndex = index;
+                        vm.accountDropDown.unshift(a)
+                    } else {
+                        vm.accountDropDown.push(a)
+                    }
+
+                });
+
+                if (!$stateParams.backAction) {
+                    if (accounts.length > 0) {
+                        vm.transactionModel.payerAccountId = accounts[loanAccountIndex].accountId;
+                        vm.transactionModel.payerAccountNo = accounts[loanAccountIndex].accountNo;
+                        vm.tradingpartnerInfoModel.available = accounts[loanAccountIndex].remainingAmount - accounts[loanAccountIndex].pendingAmount;
+                        vm.tradingpartnerInfoModel.tenor = accounts[loanAccountIndex].tenor;
+                        vm.tradingpartnerInfoModel.interestRate = accounts[loanAccountIndex].interestRate;
+                    }
+                }
+
+                if (accounts.length > 0 && accounts[loanAccountIndex].accountType == 'LOAN') {
+                    vm.transactionModel.transactionMethod = 'TERM_LOAN';
+                    vm.isLoanPayment = true;
+                    _loadMaturityDate();
+                }
+
+            }).catch(function(response) {
+                log.error(response);
+            });
+        }
+        
+        var init = function() {
+        	_loadAccount(ownerId, vm.criteria.supplierId);
         }();
     }
 ]);
