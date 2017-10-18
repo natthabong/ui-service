fa0efe569a9a3969c1fc43fe799d7c25ca35eaf8
GEC\tapanapong.kha <tapanapong.kha@gec.co.th>
2017-10-18 18:01:24 +0700

    [CHANGE] update create payment woip

--- src/main/resources/static/js/app/modules/transaction/payment/controllers/ValidateSubmitController.js
+++ src/main/resources/static/js/app/modules/transaction/payment/controllers/ValidateSubmitController.js
@@ -8,12 +8,13 @@ paymentModule.controller('ValidateSubmitController', [
     '$timeout',
     'SCFCommonService',
     'TransactionService',
-    function($scope, $stateParams, UIFactory, PageNavigation,
+    function ($scope, $stateParams, UIFactory, PageNavigation,
         PagingController, $timeout, SCFCommonService, TransactionService) {
 
         var vm = this;
         vm.transactionModel = $stateParams.transactionModel;
         vm.tradingpartnerInfoModel = $stateParams.tradingpartnerInfoModel;
+        $scope.validateDataFailPopup = false;
 
         vm.isCreateTransWithInvoice = true;
         if (vm.tradingpartnerInfoModel.createTransactionType !== undefined && vm.tradingpartnerInfoModel.createTransactionType == 'WITHOUT_INVOICE') {
@@ -31,71 +32,115 @@ paymentModule.controller('ValidateSubmitController', [
             columns: []
         };
 
-        var loadDocumentDisplayConfig = function(ownerId) {
+        var loadDocumentDisplayConfig = function (ownerId) {
             var deffered = SCFCommonService.getDocumentDisplayConfig(ownerId, 'RECEIVABLE', 'TRANSACTION_DOCUMENT');
-            deffered.promise.then(function(response) {
+            deffered.promise.then(function (response) {
                 vm.dataTable.columns = response.items;
             });
         }
 
-        vm.searchDocument = function(pagingModel) {
+        vm.searchDocument = function (pagingModel) {
             vm.pagingController.search(pagingModel);
         }
 
-        vm.backToCreate = function() {
-            $timeout(function() {
+        vm.backToCreate = function () {
+            $timeout(function () {
                 PageNavigation.backStep();
             }, 10);
         };
 
-        vm.createNewAction = function() {
-            $timeout(function() {
+        vm.createNewAction = function () {
+            $timeout(function () {
                 PageNavigation.backStep(true);
             }, 10);
         };
 
-        vm.viewRecent = function() {
-            $timeout(function() {
+        vm.viewRecent = function () {
+            $timeout(function () {
                 PageNavigation.gotoPage('/payment-transaction/view', { transactionModel: vm.transactionModel, isShowViewHistoryButton: true, viewMode: 'MY_ORGANIZE' });
             }, 10);
         };
 
-        vm.viewHistory = function() {
-            $timeout(function() {
+        vm.viewHistory = function () {
+            $timeout(function () {
                 PageNavigation.gotoPage('/my-organize/payment-transaction');
             }, 10);
         };
 
-        vm.homeAction = function() {
-            $timeout(function() {
+        vm.homeAction = function () {
+            $timeout(function () {
                 PageNavigation.gotoPage('/home');
             }, 10);
         };
 
-        vm.submitTransaction = function() {
-            vm.transactionModel.transactionDate = SCFCommonService.convertStringTodate(vm.transactionModel.transactionDate);
-            vm.transactionModel.maturityDate = SCFCommonService.convertStringTodate(vm.transactionModel.maturityDate);
-            vm.transactionModel.documents.forEach(function(document) {
-                document.paymentAmount = document.netAmount;
-                document.paymentDate = vm.transactionModel.transactionDate;
-            });
-            var deffered = TransactionService.submitTransaction(vm.transactionModel);
-            deffered.promise.then(function(response) {
-                var storeAccount = vm.transactionModel.payerAccountNo;
-                vm.transactionModel = response.data;
-                if (storeAccount == 'LOAN') {
-                    vm.transactionModel.payerAccountNo = 'LOAN';
+        var validatePaymentAmount = function () {
+            var valid = true;
+            if (vm.transactionModel.transactionAmount <= 0) {
+                valid = false;
+                vm.errorMsgPopup = "Transaction amount must be greater than zero";
+                $scope.validateDataFailPopup = true;
+            }
+            return valid;
+        }
+
+        vm.submitTransaction = function () {
+            if (vm.isCreateTransWithInvoice) {
+                vm.transactionModel.transactionDate = SCFCommonService.convertStringTodate(vm.transactionModel.transactionDate);
+                vm.transactionModel.maturityDate = SCFCommonService.convertStringTodate(vm.transactionModel.maturityDate);
+
+                vm.transactionModel.documents.forEach(function (document) {
+                    document.paymentAmount = document.netAmount;
+                    document.paymentDate = vm.transactionModel.transactionDate;
+                });
+                vm.transactionModel.createTransactionType = "WITH_INVOICE";
+                var deffered = TransactionService.submitTransaction(vm.transactionModel);
+                deffered.promise.then(function (response) {
+                    var storeAccount = vm.transactionModel.payerAccountNo;
+                    vm.transactionModel = response.data;
+                    if (storeAccount == 'LOAN') {
+                        vm.transactionModel.payerAccountNo = 'LOAN';
+                    }
+                    vm.transactionNo = vm.transactionModel.transactionNo;
+                    $scope.confirmPopup = false;
+                    $scope.validateDataPopup = true;
+                }).catch(function (response) {
+                    $scope.submitFailPopup = true;
+                    vm.errorMsgPopup = response.data.errorCode;
+                });
+            } else {
+                if (validatePaymentAmount()) {
+                    vm.transactionModel.transactionDate = SCFCommonService.convertStringTodate(vm.transactionModel.transactionDate);
+                    vm.transactionModel.maturityDate = SCFCommonService.convertStringTodate(vm.transactionModel.maturityDate);
+
+                    vm.transactionModel.documents.forEach(function (document) {
+                        document.documentId = null;
+                        document.documentNo = null;
+                        document.documentType = null;
+                        document.customerCode = null;
+                        document.paymentAmount = document.netAmount;
+                        document.paymentDate = vm.transactionModel.transactionDate;
+                    });
+                    vm.transactionModel.createTransactionType = "WITHOUT_INVOICE";
+                    var deffered = TransactionService.submitTransaction(vm.transactionModel);
+                    deffered.promise.then(function (response) {
+                        var storeAccount = vm.transactionModel.payerAccountNo;
+                        vm.transactionModel = response.data;
+                        if (storeAccount == 'LOAN') {
+                            vm.transactionModel.payerAccountNo = 'LOAN';
+                        }
+                        vm.transactionNo = vm.transactionModel.transactionNo;
+                        $scope.confirmPopup = false;
+                        $scope.validateDataPopup = true;
+                    }).catch(function (response) {
+                        $scope.submitFailPopup = true;
+                        vm.errorMsgPopup = response.data.errorCode;
+                    });
                 }
-                vm.transactionNo = vm.transactionModel.transactionNo;
-                $scope.confirmPopup = false;
-                $scope.validateDataPopup = true;
-            }).catch(function(response) {
-                $scope.submitFailPopup = true;
-                vm.errorMsgPopup = response.data.errorCode;
-            });
+            }
+
         };
 
-        var init = function() {
+        var init = function () {
             if (vm.isCreateTransWithInvoice) {
                 vm.pagingController.search();
                 loadDocumentDisplayConfig(vm.transactionModel.supplierId);
@@ -105,6 +150,6 @@ paymentModule.controller('ValidateSubmitController', [
             if (vm.transactionModel.payerAccountNo != 'LOAN') {
                 vm.isLoanPayment = false;
             }
-        }();
+        } ();
     }
 ]);
