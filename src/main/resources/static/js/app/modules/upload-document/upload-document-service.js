92ae9bbb2cf0078a91166ec10a467cdb22238e90
GEC\cheerakwan.sut <cheerakwan.sut@gec.co.th>
2017-10-24 14:02:46 +0700

    [FIXED] case channel is inactive don't show upload document page

--- src/main/resources/static/js/app/modules/upload-document/upload-document-service.js
+++ src/main/resources/static/js/app/modules/upload-document/upload-document-service.js
@@ -1,65 +1,66 @@
-angular.module('scfApp').factory('UploadDocumentService',['$http', '$q', 'blockUI', UploadDocumentService]);
-function UploadDocumentService($http, $q, blockUI){
-	return {
-		upload: upload,
-		getFileType: getFileType,
-		confirmUpload: confirmUpload,
-		verifyChannel: verifyChannel
-	}
-	function upload(fileModel){
-	    	blockUI.start();
-		var deffered = $q.defer();
-		var formData = new FormData();
-		formData.append('file', fileModel.file);
-		formData.append('fileConfigId', fileModel.fileConfigId);
-		$http.post('/api/upload-document/upload', formData, {
-			transformRequest: angular.identity,
-			headers:{'Content-type': undefined}
-		}).then(function(response){
-		    	blockUI.stop();
-			deffered.resolve(response);
-		}).catch(function(response){
-		    	blockUI.stop();
-			deffered.reject(response);
-		});
-		return deffered;
-	}
-	
-	function getFileType(processType){
-		var deffered = $q.defer();		
-		$http.get('/api/upload-document/file-types?processType='+processType)
-		.then(function(response){
-			console.log(response);
-			deffered.resolve(response);
-		}).catch(function(response){
-			deffered.reject('Get file type fail');
-		});
-		return deffered;
-	}
-	
-	function confirmUpload(uploadConfirmPayload){
-		var deffered = $q.defer();		
-		$http({
+angular.module('scfApp').factory('UploadDocumentService', ['$http', '$q', 'blockUI', UploadDocumentService]);
+
+function UploadDocumentService($http, $q, blockUI) {
+    return {
+        upload: upload,
+        getFileType: getFileType,
+        confirmUpload: confirmUpload,
+        verifyChannel: verifyChannel
+    }
+
+    function upload(fileModel) {
+        blockUI.start();
+        var deffered = $q.defer();
+        var formData = new FormData();
+        formData.append('file', fileModel.file);
+        formData.append('fileConfigId', fileModel.fileConfigId);
+        $http.post('/api/upload-document/upload', formData, {
+            transformRequest: angular.identity,
+            headers: { 'Content-type': undefined }
+        }).then(function(response) {
+            blockUI.stop();
+            deffered.resolve(response);
+        }).catch(function(response) {
+            blockUI.stop();
+            deffered.reject(response);
+        });
+        return deffered;
+    }
+
+    function getFileType(processType) {
+        var deffered = $q.defer();
+        $http.get('/api/upload-document/file-types?processType=' + processType)
+            .then(function(response) {
+                deffered.resolve(response);
+            }).catch(function(response) {
+                deffered.reject('Get file type fail');
+            });
+        return deffered;
+    }
+
+    function confirmUpload(uploadConfirmPayload) {
+        var deffered = $q.defer();
+        $http({
             method: 'POST',
             url: '/api/upload-document/confirm-import',
             data: uploadConfirmPayload
-        }).then(function(response){
-			deffered.resolve(response);
-		}).catch(function(response){
-			deffered.reject(response);
-		});
-		return deffered;
-	}
-	
-	function verifyChannel(channel){
-		var deffered = $q.defer();
-		$http.post('/api/v1/channels/'+channel+'/verify', {
-		    
-		}).then(function(response){
-			deffered.resolve(response);
-		}).catch(function(response){
-			deffered.reject(response);
-		});
-		return deffered;
-	}
+        }).then(function(response) {
+            deffered.resolve(response);
+        }).catch(function(response) {
+            deffered.reject(response);
+        });
+        return deffered;
+    }
+
+    function verifyChannel(channel) {
+        var deffered = $q.defer();
+        $http.post('/api/v1/channels/' + channel + '/verify', {
+
+        }).then(function(response) {
+            deffered.resolve(response);
+        }).catch(function(response) {
+            deffered.reject(response);
+        });
+        return deffered;
+    }
 }
