2eca5327fff20ca325b73f8430a8f57b06b25e44
GEC\phongsathon.kon <phongsathon.kon@gec.co.th>
2017-10-25 10:47:22 +0700

    add supplier credit information - not complete

---
+++ src/main/resources/static/js/app/modules/supplier-credit-information/controllers/SupplierCreditInformationController.js
@@ -0,0 +1,63 @@
+'use strict';
+var sciModule = angular.module('gecscf.supplierCreditInformation');
+sciModule.controller('SupplierCreditInformationController',[
+	'$scope',
+	'$stateParams',
+	'UIFactory',
+	'PagingController',
+	'SupplierCreditInformationService',
+	'SCFCommonService',
+	'$http',
+	function($scope,$stateParams, UIFactory,PagingController,SupplierCreditInformationService,SCFCommonService,$http) {
+		var vm = this;
+		
+		vm.buyer = $stateParams.buyer || null;
+		vm.supplier = $stateParams.supplier || null;
+		
+		vm.data = [];
+		
+		vm.search = function(){
+			var buyer = undefined;
+			var supplier = undefined;
+			if(angular.isObject(vm.buyer)){
+				if(angular.isObject(vm.supplier)){
+					buyer = vm.buyer.organizeId;
+					supplier = vm.supplier.organizeId;
+				} else {
+					buyer = vm.buyer.organizeId;
+				}
+			} else if(angular.isObject(vm.supplier)){
+				supplier = vm.supplier.organizeId;
+			} else {
+				buyer = null;
+				supplier = null;
+			}
+			var dataSource = $http({url:'/api/v1/supplier-credit-information', method: 'GET',params: {buyerId:buyer,supplierId:supplier}});
+			dataSource.success(function(response) {						
+				vm.data = response.content;
+			});
+			//vm.data = SupplierCreditInformationService.getCreditInformation(buyer,supplier);
+		};
+	
+		// Organize auto suggestion model.
+		var _organizeTypeHead = function(q) {
+			q = UIFactory.createCriteria(q);
+			return SupplierCreditInformationService.getOrganizeByNameOrCodeLike(q);
+		}
+
+		vm.organizeAutoSuggestModel = UIFactory.createAutoSuggestModel({
+			placeholder : 'Enter organize name or code',
+			itemTemplateUrl : 'ui/template/autoSuggestTemplate.html',
+			query : _organizeTypeHead
+		});
+
+		// Main of program
+		var initLoad = function() {
+			//vm.search();
+		}();
+		
+		vm.decodeBase64 = function(data){
+			return atob(data);
+		};
+
+	} ]);
---
+++ src/main/resources/static/js/app/modules/supplier-credit-information/supplier-credit-information.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('gecscf.supplierCreditInformation', [ 'ui.router', 'gecscf.ui' ]).config(
+		[ '$stateProvider', function($stateProvider) {
+			
+		} ]);
