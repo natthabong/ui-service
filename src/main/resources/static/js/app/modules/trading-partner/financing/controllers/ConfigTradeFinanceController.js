c067f2b07dae7498f9598b939aecb99933860f0e
GEC\tapanapong.kha <tapanapong.kha@gec.co.th>
2017-12-13 17:42:11 +0700

    [NEW] create set default loan acc and disable delete button

--- src/main/resources/static/js/app/modules/trading-partner/financing/controllers/ConfigTradeFinanceController.js
+++ src/main/resources/static/js/app/modules/trading-partner/financing/controllers/ConfigTradeFinanceController.js
@@ -41,12 +41,29 @@ tradeFinanceModule.controller('ConfigTradeFinanceController',['$scope','$statePa
 						return ($filter('accountNoDisplay')(record.accountNo));
 					}
 				},{
+					fieldName : 'defaultLoanNo',
+					field : 'defaultLoanNo',
+					label : 'Default account',
+					idValueField : 'template',
+					id : 'default-loan-no-{value}-label',
+					sortable : false,
+					cellTemplate: '<img	style="height: 16px; width: 16px;" ng-show="data.defaultLoanNo" data-ng-src="img/checkmark.png"/>',
+				},{
 					cssTemplate : 'text-center',
 					sortable : false,
 					cellTemplate : '<scf-button id="{{$parent.$index + 1}}-view-button" class="btn-default gec-btn-action" ng-disabled="!ctrl.canView" ng-click="ctrl.view(data)" title="View"><i class="fa fa-search" aria-hidden="true"></i></scf-button>'
 								+ '<scf-button id="{{$parent.$index + 1}}-edit-button" class="btn-default gec-btn-action" ng-disabled="!ctrl.canManage" ng-click="ctrl.edit(data)" title="Edit"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></scf-button>'
-								+ '<scf-button id="{{$parent.$index + 1}}-delete-button" class="btn-default gec-btn-action" ng-disabled="!ctrl.canManage" ng-click="ctrl.deleteTradeFinance(data)" title="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i></scf-button>'
+								+ '<scf-button id="{{$parent.$index + 1}}-delete-button" class="btn-default gec-btn-action" ng-disabled="!ctrl.canManage || data.defaultLoanNo || ctrl.isSupplier(data.borrowerType)" ng-click="ctrl.deleteTradeFinance(data)" title="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i></scf-button>'
+								+ '<scf-button id="{{$parent.$index + 1}}-set-default-button" class="btn-default gec-btn-action" ng-click="ctrl.setDefaultCode(data)" title="Set default"><i class="fa fa-check-square-o" aria-hidden="true"></i></scf-button>'
 				} ]
+		}
+
+		vm.isSupplier = function(data){
+			var disable = false;
+			if(data === "SUPPLIER"){
+				disable = true;
+			}
+			return disable;
 		}
 
 		var storeCriteria = function(){
