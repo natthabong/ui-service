ce5128a0fcf4e80590822408db378bcd1d44e4ea
GEC\samacharn.san <Samacharn.Sankul@outlook.com>
2017-12-15 18:53:38 +0700

    [ADD] Add product type table.

--- src/main/resources/static/js/app/modules/organize/configuration/product-type/controllers/ProductTypeListController.js
+++ src/main/resources/static/js/app/modules/organize/configuration/product-type/controllers/ProductTypeListController.js
@@ -1,54 +1,85 @@
 'use strict';
 var productTypeModule = angular.module('gecscf.organize.configuration.productType');
 productTypeModule.controller('ProductTypeListController', [
-		'Service',
-		'UIFactory',
 		'PageNavigation',
 		'PagingController',
+		'Service',
+		'UIFactory',
 		'ngDialog',
 		'$http',
+		'$log',
 		'$q',
 		'$rootScope',
 		'$scope',
 		'$stateParams',
-		'$log',
-		function(Service, UIFactory, PageNavigation, PagingController, ngDialog, $http, $q, $rootScope, $scope, $stateParams, $log) {
+		function(PageNavigation, PagingController, Service, UIFactory, ngDialog, $http, $log, $q, $rootScope, $scope, $stateParams) {
 			var vm = this;
 			var log = $log;
-			var organizeId = $rootScope.userInfo.organizeId;
 
 			vm.manageAllConfig = false;
 			vm.manageMyOrgConfig = false;
-			
-			vm.pageModel = {
-				pageSizeSelectModel : '10',
-				totalRecord : 0,
-				currentPage : 0,
-				clearSortOrder : false,
-				page : 0,
-				pageSize : 10
+
+			vm.dataTable = {
+				identityField:'customerCode',
+				columns : [
+					{
+						fieldName : '$rowNo',
+						labelEN : 'No.',
+						labelTH : 'ลำดับ',
+						sortable : true,
+						id : '$rowNo-{value}',
+						filterType : 'translate',
+						cssTemplate : 'text-right'
+					},
+					{
+						fieldName : 'productType',
+						labelEN : 'Product type',
+						labelTH : 'ประเภทสินค้า',
+						sortable : true,
+						id : 'product-type-{value}',
+						filterType : 'translate',
+						cssTemplate : 'text-left'
+					},
+					{
+						fieldName : 'name',
+						labelEN : 'Name',
+						labelTH : 'ชื่อ',
+						sortable : true,
+						id : 'name-{value}',
+						filterType : 'translate',
+						cssTemplate : 'text-left'
+					},
+					{
+						labelEN : '',
+						labelTH : '',
+						sortable : false,
+						cssTemplate : 'text-left',
+						cellTemplate : '<scf-button id="{{data.customerCode}}-edit-button" class="btn-default gec-btn-action" ng-disabled="ctrl.unauthen()" ng-click="ctrl.customerCodeSetup(data)" title="Setup customer code"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></scf-button>' +
+							'<scf-button id="{{data.customerCode}}-delete-button"  class="btn-default gec-btn-action" ng-disabled="ctrl.unauthen()" ng-click="ctrl.deleteCustomerCode(data)" title="Delete customer code"><i class="fa fa-trash-o" aria-hidden="true"></i></scf-button>'
+					}
+				]
 			};
-			
+
 			vm.gotoListPage = function() {
 				var params = {
 					organizeId : $scope.sponsorId
 				};
-				
+
 				PageNavigation.gotoPage('/customer-organize/product-types', params);
 			}
-			
+
 			vm.gotoSetupPage = function() {
 				var params = {
 						organizeId : $scope.sponsorId
 				};
-				
+
 				PageNavigation.gotoPage('/customer-organize/product-types/setup', params);
 			}
-			
-			vm.initLoad = function() {	
-				var url = '/api/v1/organize-customers/'+ organizeId + '/product-types';
+
+			vm.initLoad = function() {
+				var organizeId = $rootScope.userInfo.organizeId;
+				var url = '/api/v1/organize-customers/'+ 'YAMAHA_CO' + '/product-types';
 				vm.pagingController = PagingController.create(url, null, 'GET');
-				
 				console.log(vm.pagingController);
 			}
 
@@ -60,8 +91,8 @@ productTypeModule.controller('ProductTypeListController', [
 					return true;
 				}
 			}
-			
+
 			vm.initLoad();
-		} 
+		}
 	]
 );
