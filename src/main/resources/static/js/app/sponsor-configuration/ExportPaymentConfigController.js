7ee8e814617f9a14a173fd3eb63f214dd9a734e4
GEC\atithat.dan <atithat.dan@gec.co.th>
2018-02-01 20:54:25 +0700

    commit delete export layout

--- src/main/resources/static/js/app/sponsor-configuration/ExportPaymentConfigController.js
+++ src/main/resources/static/js/app/sponsor-configuration/ExportPaymentConfigController.js
@@ -7,9 +7,11 @@ angular.module('scfApp').controller('ExportPaymentConfigController', [
 	'$timeout',
 	'PageNavigation',
 	'Service',
+	'FileLayoutService',
+	'UIFactory', 
 	'ConfigurationUtils',
 	function($log, $scope, $state, SCFCommonService, $stateParams, $timeout,
-			PageNavigation, Service , ConfigurationUtils) {
+			PageNavigation, Service, FileLayoutService, UIFactory , ConfigurationUtils) {
         var vm = this;
         var log = $log;
         vm.viewAll = false;
@@ -65,9 +67,50 @@ angular.module('scfApp').controller('ExportPaymentConfigController', [
 			});
 		}
         
-        vm.deleteExportLayout = function(data) {
-			PageNavigation.gotoPage('/');
-		}
+        vm.deleteLayout = function(data) {
+            UIFactory
+            .showConfirmDialog({
+              data : {
+                headerMessage : 'Confirm delete?'
+              },
+              confirm : function() {
+                return FileLayoutService
+                    .deleteLayout(data, vm.processType, vm.integrateType);
+              },
+              onFail : function(response) {
+                var status = response.status;
+                  var msg = {
+                    404 : "File layout has been deleted.",
+                    409 : "File layout has been modified.",
+                    405 : "File Layout has been used."
+                  }
+                  UIFactory
+                      .showFailDialog({
+                        data : {
+                          headerMessage : 'Delete file layout fail.',
+                          bodyMessage : msg[status] ? msg[status]
+                              : response.errorMessage
+                        },
+                        preCloseCallback : function(){
+                          callService(vm.processType, vm.integrateType);
+                        }
+                      });
+
+              },
+              onSuccess : function(response) {
+                UIFactory
+                    .showSuccessDialog({
+                      data : {
+                        headerMessage : 'Delete file layout success.',
+                        bodyMessage : ''
+                      },
+                      preCloseCallback : function(){
+                        callService(vm.processType, vm.integrateType);
+                      }
+                    });
+              }
+            });
+      }
         
         function callService(processType, integrateType){
 			var sponsorId = organizeId;
