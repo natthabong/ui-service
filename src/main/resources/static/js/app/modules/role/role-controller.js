1eba30a98c884f5f733e8075abc2b4cadf733dff
GEC\natthapong.noo <natthapong.noo@gec.co.th>
2018-02-09 16:05:01 +0700

    [FIXED] Role Page.
    - Change selection method.

--- src/main/resources/static/js/app/modules/role/role-controller.js
+++ src/main/resources/static/js/app/modules/role/role-controller.js
@@ -17,35 +17,28 @@ angular.module('scfApp').controller('RoleController',['$scope','Service', '$stat
         vm.viewMode = false;
         vm.headerMessage;
         vm.roleName;
+        vm.model = {
+            roleId : null,
+            roleName :null,
+            privileges : [],
+            version:null  
+        };
         vm.roleMessageError;
         var defaultPrivilegeListIsSelected =[];
 
         $scope.cancel = function() {
             PageNavigation.gotoPreviousPage();
         }
-
-        vm.privilegeGroupList = [];
-        vm.setUpPrivilegeGroupList = [];
+        
+        $scope.selectedPrivileges = [];
+        
+        vm.privilegeGroups = [];
 
         var rolePrivilegeModel = {
             roleId : null,
             roleName :null,
             privileges : [],
             version:null
-        }
-
-        var getValueRolePrivilegeIsSelected = function(){
-            var privilegeList = [];
-            for(var i=0;i<vm.privilegeGroupList.length;i++){
-                for(var j=0;j<vm.privilegeGroupList[i].privileges.length;j++){
-                    if(vm.privilegeGroupList[i].privileges[j].value){
-                        privilegeList.push({
-                            privilegeId : vm.privilegeGroupList[i].privileges[j].privilegeId
-                        });
-                    }
-                }
-            }
-            return privilegeList;
         }
 
         var _save = function(data){
@@ -95,7 +88,7 @@ angular.module('scfApp').controller('RoleController',['$scope','Service', '$stat
                         headerMessage : 'Confirm save?'
                     },
                     confirm : function() {
-                        return _save(rolePrivilegeModel);
+                        return _save(vm.model);
                     },
                     onFail : function(response) {
                         if(response.data == 403){
@@ -140,13 +133,13 @@ angular.module('scfApp').controller('RoleController',['$scope','Service', '$stat
                 vm.roleMessageError = "Role name is required";
                 $scope.error.roleNameIsRequired = true;
             }else{
-                rolePrivilegeModel.roleName = vm.roleName;
+                vm.model.roleName = vm.roleName;
                 $scope.error.roleNameIsRequired = false;
             }
 
-            rolePrivilegeModel.privileges = getValueRolePrivilegeIsSelected();
+// vm.model.privileges = getValueRolePrivilegeIsSelected();
 
-            if(rolePrivilegeModel.privileges.length == 0){
+            if(vm.model.privileges.length == 0){
                 validate = false;
                 $scope.error.permissionsIsRequired = true;
             }else{
@@ -154,103 +147,46 @@ angular.module('scfApp').controller('RoleController',['$scope','Service', '$stat
             }
             return validate;
         }
-
-        vm.checkDependValue = function(privilege){
-            if(privilege.value){
-                if(privilege.dependencies.length > 0){
-                    for(var i=0;i<privilege.dependencies.length;i++){
-                        for(var groupIndex=0;groupIndex<vm.privilegeGroupList.length;groupIndex++){
-                            for(var privilegeIndex=0;privilegeIndex<vm.privilegeGroupList[groupIndex].privileges.length;privilegeIndex++){
-                                if(vm.privilegeGroupList[groupIndex].privileges[privilegeIndex].privilegeId === privilege.dependencies[i].privilegeId){
-                                    if(vm.privilegeGroupList[groupIndex].privileges[privilegeIndex].isDisable == false){
-                                        vm.privilegeGroupList[groupIndex].privileges[privilegeIndex].isDisable = true;
-                                        vm.privilegeGroupList[groupIndex].privileges[privilegeIndex].value = true;
-                                    }
-                                    vm.checkDependValue(vm.privilegeGroupList[groupIndex].privileges[privilegeIndex]);
-                                }
-                            }
-                        }
-                    }
-                }
-            }else{
-                if(privilege.dependencies.length > 0){
-                    for(var i=0;i<privilege.dependencies.length;i++){
-                        for(var groupIndex=0;groupIndex<vm.privilegeGroupList.length;groupIndex++){
-                            for(var privilegeIndex=0;privilegeIndex<vm.privilegeGroupList[groupIndex].privileges.length;privilegeIndex++){
-                                if(vm.privilegeGroupList[groupIndex].privileges[privilegeIndex].privilegeId === privilege.dependencies[i].privilegeId){
-                                    if(vm.privilegeGroupList[groupIndex].privileges[privilegeIndex].isDisable == true){
-                                        vm.privilegeGroupList[groupIndex].privileges[privilegeIndex].isDisable = false;
-                                        vm.privilegeGroupList[groupIndex].privileges[privilegeIndex].value = true;
-                                    }
-                                    vm.checkDependValue(vm.privilegeGroupList[groupIndex].privileges[privilegeIndex]);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        var comparePrivilegeIsSelected = function(privilegeId){
-            var invalid = defaultPrivilegeListIsSelected.indexOf(privilegeId);
-            if(invalid != -1){
-                return true;
-            }
-            return false;
-        }
-
-        var indexOfPrivilegeIsSelected = [];
-
-        var defualtValuePrivilegeGroupList = function(data){
-            for(var i=0;i<data.length;i++){
-                for(var j=0; j<data[i].privileges.length;j++){
-                    data[i].privileges[j].value = false;
-                    data[i].privileges[j].isDisable = false;
-                    if(defaultPrivilegeListIsSelected != []){
-                        data[i].privileges[j].value = comparePrivilegeIsSelected(data[i].privileges[j].privilegeId);
-                        if(data[i].privileges[j].value){
-                            indexOfPrivilegeIsSelected.push({
-                                groupIndex : i,
-                                privilegeIndex : j
-                            });
-                        }
-                    }
-                }
-            }
-            return data;
-        }
-
+        
         var initialPrivilegeGroup = function(){
             var url = '/api/v1/privilege-groups';
             var defered = Service.doGet(url,null);
             defered.promise.then(function(response){
-                vm.privilegeGroupList = defualtValuePrivilegeGroupList(response.data);
-                if(indexOfPrivilegeIsSelected != []){
-                    for(var index=0; index<indexOfPrivilegeIsSelected.length; index++){
-                        var groupIndex = indexOfPrivilegeIsSelected[index].groupIndex;
-                        var privilegeIndex = indexOfPrivilegeIsSelected[index].privilegeIndex;
-                        vm.checkDependValue(vm.privilegeGroupList[groupIndex].privileges[privilegeIndex]);
-                    }
-                }
+                vm.privilegeGroups =  response.data ;
             }).catch(function(response) {
                 log.error('Get role group fail');
             });
         }
 
-        var initialRoleInformation = function(model){
+        var initialRoleInformation = function(){
             var roleId = $stateParams.roleId;
             RoleService.getRole(roleId).promise.then(function(response) {
-              model = response.data;
-              model.privileges.forEach(function(p){
-                defaultPrivilegeListIsSelected.push(p.privilegeId);
-              });
-              vm.roleName = model.roleName;
+              vm.model = response.data;
+              if(vm.model.privileges!=null){
+                vm.model.privileges.forEach(function(p){
+                  $scope.selection.push(p.privilegeId);
+                });
+              }
             }).catch(function(response) {
                log.error('Save role fail');
                return deferred.reject(response);
             });
         }
-
+        
+    $scope.selection = [];
+    $scope.toggleSelection =  function(privilege){
+      if(vm.model.privileges==null){
+        vm.model.privileges = [];
+      }
+      
+      if (vm.model.privileges.indexOf(privilege) === -1) {
+        vm.model.privileges.push(privilege);
+      } else {
+        vm.model.privileges.splice(vm.model.privileges.indexOf(privilege), 1);
+      }
+      console.log( vm.model);
+    }
+    
 		var initial = function(){
             if(mode!=""){
                 if(mode === 'NEW'){
@@ -258,12 +194,12 @@ angular.module('scfApp').controller('RoleController',['$scope','Service', '$stat
                     vm.headerMessage = page.NEW;
                 }else if(mode === 'EDIT'){
                     vm.headerMessage = page.EDIT;
-                    initialRoleInformation(rolePrivilegeModel);
+                    initialRoleInformation();
                 }else if(mode === 'VIEW'){
                     vm.button = 'Back';
                     vm.viewMode = true;
                     vm.headerMessage = page.VIEW;
-                    initialRoleInformation(rolePrivilegeModel);
+                    initialRoleInformation();
                 }
                 initialPrivilegeGroup();
             }else{
