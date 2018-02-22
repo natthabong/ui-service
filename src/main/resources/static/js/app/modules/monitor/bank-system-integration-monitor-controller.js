f309e62d3ecc27cdceb75228d364a31c58350603
yongyuth.sam <pajupaja@gmail.com>
2018-02-22 20:18:54 +0700

    [FIXED] Funding monitor (GEC Website)

--- src/main/resources/static/js/app/modules/monitor/bank-system-integration-monitor-controller.js
+++ src/main/resources/static/js/app/modules/monitor/bank-system-integration-monitor-controller.js
@@ -5,13 +5,35 @@ scfApp.controller('BankSystemIntegrationMonitorController', [ '$scope', 'Service
 	function($scope, Service, $stateParams, $log, UIFactory, $q, $rootScope, $http, PageNavigation, SystemIntegrationMonitorService, ngDialog) {
 
         var vm = this; 
-        vm.organize = {
-			organizeId : null,
-			organizeName : null
-		};
-
         vm.check = function(){
             $scope.$broadcast('onload');
+        }
+        vm.funding = {
+			fundingId : null,
+			fundingName : null
+		};
+
+        vm.fundingDropdown = [];
+        vm.searchFundings = function(){
+       	 var serviceDiferred = Service.doGet('api/v1/fundings');
+            var failedFunc = function(response) {
+                log.error('Load data error');
+            };
+            var successFunc = function(response) {
+	           	var _fundings = response.data;
+	           	if (angular.isDefined(_fundings)) {
+	           		vm.funding.fundingId = _fundings[0].fundingId;
+	           		vm.funding.fundingName = _fundings[0].fundingName;
+	           		_fundings.forEach(function (funding) {
+	                      var selectObj = {
+	                          label: funding.fundingName,
+	                          value: funding.fundingId
+	                      }
+	                      vm.fundingDropdown.push(selectObj);
+	                   });
+	          	  }
+            };
+            serviceDiferred.promise.then(successFunc).catch(failedFunc);
         }
 
 //        var getBankCode = function(){
@@ -22,19 +44,20 @@ scfApp.controller('BankSystemIntegrationMonitorController', [ '$scope', 'Service
 //			}
 //		}
 
-        var getBankProfile = function(bankCode){
-			var serviceUrl = '/api/v1/organize-customers/'+bankCode+'/profile';
-			var serviceDiferred = Service.doGet(serviceUrl, {});		
-			serviceDiferred.promise.then(function(response){
-				vm.organize.organizeId= response.data.organizeId;
-				vm.organize.organizeName = response.data.organizeName;
-			}).catch(function(response){
-				log.error('Load customer code group data error');
-			});
-		}
+//        var getBankProfile = function(bankCode){
+//			var serviceUrl = '/api/v1/organize-customers/'+bankCode+'/profile';
+//			var serviceDiferred = Service.doGet(serviceUrl, {});		
+//			serviceDiferred.promise.then(function(response){
+//				vm.organize.organizeId= response.data.organizeId;
+//				vm.organize.organizeName = response.data.organizeName;
+//			}).catch(function(response){
+//				log.error('Load customer code group data error');
+//			});
+//		}
 
         var initial = function(){
-            getBankProfile($rootScope.userInfo.fundingId);
+        	vm.searchFundings();
+//            getBankProfile($rootScope.userInfo.fundingId);
         }
         initial();
 } ]);
