39fb6009a1fe1d01bb7b47839c9fcffb78aea1ad
GEC\samacharn.san <samacharn.san@gec.co.th>
2018-02-23 18:17:46 +0700

    [FIX] Fix "Chirstian calendar" wording.

--- src/main/resources/static/js/app/modules/organize/configuration/file-layout/services/FileLayerExampleDisplayService.js
+++ src/main/resources/static/js/app/modules/organize/configuration/file-layout/services/FileLayerExampleDisplayService.js
@@ -1,55 +1,54 @@
 'use strict';
-var module = angular.module('gecscf.organize.configuration.fileLayout');
-module.factory('FileLayerExampleDisplayService', ['$filter', function($filter) {
-	
+angular.module('gecscf.organize.configuration.fileLayout').factory('FileLayerExampleDisplayService', ['$filter', function ($filter) {
+
 	return {
-		TEXT_DisplayExample : TEXT_DisplayExample,
-		DOCUMENT_NO_DisplayExample : DOCUMENT_NO_DisplayExample,
-		CUSTOMER_CODE_DisplayExample : CUSTOMER_CODE_DisplayExample,
-		DATE_TIME_DisplayExample : DATE_TIME_DisplayExample,
-		NUMERIC_DisplayExample : NUMERIC_DisplayExample,
-		PAYMENT_AMOUNT_DisplayExample : PAYMENT_AMOUNT_DisplayExample,
-		DOCUMENT_TYPE_DisplayExample : DOCUMENT_TYPE_DisplayExample,
-		RECORD_TYPE_DisplayExample : RECORD_TYPE_DisplayExample,
-		FILLER_DisplayExample : FILLER_DisplayExample,
-		SIGN_FLAG_DisplayExample : SIGN_FLAG_DisplayExample,
-		MATCHING_REF_DisplayExample : MATCHING_REF_DisplayExample
+		TEXT_DisplayExample: TEXT_DisplayExample,
+		DOCUMENT_NO_DisplayExample: DOCUMENT_NO_DisplayExample,
+		CUSTOMER_CODE_DisplayExample: CUSTOMER_CODE_DisplayExample,
+		DATE_TIME_DisplayExample: DATE_TIME_DisplayExample,
+		NUMERIC_DisplayExample: NUMERIC_DisplayExample,
+		PAYMENT_AMOUNT_DisplayExample: PAYMENT_AMOUNT_DisplayExample,
+		DOCUMENT_TYPE_DisplayExample: DOCUMENT_TYPE_DisplayExample,
+		RECORD_TYPE_DisplayExample: RECORD_TYPE_DisplayExample,
+		FILLER_DisplayExample: FILLER_DisplayExample,
+		SIGN_FLAG_DisplayExample: SIGN_FLAG_DisplayExample,
+		MATCHING_REF_DisplayExample: MATCHING_REF_DisplayExample
 	}
 
 	function TEXT_DisplayExample(record, config, mappingDataList) {
-		if(record.itemType=='FIELD'){
+		if (record.itemType == 'FIELD') {
 			var displayMessage = config.detailExamplePattern;
 			var hasExpected = !(angular.isUndefined(record.expectedValue) || record.expectedValue === null);
 			var hasValidationType = !(angular.isUndefined(record.validationType) || record.validationType === null);
-			
+
 			displayMessage = displayMessage.replace('{required}', convertRequiredToString(record));
-			if(hasValidationType){	
-				displayMessage = displayMessage.replace('Expected value', 'Expected in');	
-				if(mappingDataList != null){
-					for (var i = 0; i < mappingDataList.length; i++) { 
-						if(mappingDataList[i].mappingDataId == record.expectedValue){
+			if (hasValidationType) {
+				displayMessage = displayMessage.replace('Expected value', 'Expected in');
+				if (mappingDataList != null) {
+					for (var i = 0; i < mappingDataList.length; i++) {
+						if (mappingDataList[i].mappingDataId == record.expectedValue) {
 							displayMessage = displayMessage.replace('{expectedValue}', mappingDataList[i].mappingDataName);
 						}
 					}
-				}else{
+				} else {
 					displayMessage = displayMessage.replace('{expectedValue}', 'Mapping data');
 				}
-				displayMessage = displayMessage.replace('{exampleData}', config.defaultExampleValue);	
-			}else{
-				displayMessage = displayMessage.replace('{expectedValue}', (hasExpected ? record.expectedValue : ''));		
-				displayMessage = displayMessage.replace('{exampleData}', (hasExpected ? record.expectedValue : config.defaultExampleValue));			
+				displayMessage = displayMessage.replace('{exampleData}', config.defaultExampleValue);
+			} else {
+				displayMessage = displayMessage.replace('{expectedValue}', (hasExpected ? record.expectedValue : ''));
+				displayMessage = displayMessage.replace('{exampleData}', (hasExpected ? record.expectedValue : config.defaultExampleValue));
 			}
-		}else if(record.itemType=='DATA'){
+		} else if (record.itemType == 'DATA') {
 			var displayValue = '';
-			if(record.defaultValue != '' && record.defaultValue !=null){
+			if (record.defaultValue != '' && record.defaultValue != null) {
 				displayValue = record.defaultValue;
 			}
-			displayMessage = 'Expected value: '+displayValue+' (Ex. '+displayValue+')';
+			displayMessage = 'Expected value: ' + displayValue + ' (Ex. ' + displayValue + ')';
 		}
-		
+
 		return displayMessage;
 	}
-	
+
 	function MATCHING_REF_DisplayExample(record, config) {
 		var displayMessage = config.detailExamplePattern;
 		var hasExpected = !(angular.isUndefined(record.expectedValue) || record.expectedValue === null);
@@ -83,7 +82,7 @@ module.factory('FileLayerExampleDisplayService', ['$filter', function($filter) {
 		}
 		var displayMessage = config.detailExamplePattern;
 
-		var calendarEra = "Christ calendar (A.D.)";
+		var calendarEra = "Gregorian calendar (A.D.)";
 		if (record.calendarEra == "B.E.") {
 			calendarEra = "Buddhist calendar (B.E.)";
 		}
@@ -91,18 +90,18 @@ module.factory('FileLayerExampleDisplayService', ['$filter', function($filter) {
 		//2016-04-13T13:30:55
 		var defaultDateExample = config.defaultExampleValue;
 		var year = defaultDateExample.substring(0, 4);
-	    var month = defaultDateExample.substring(5, 7);
-	    var day = defaultDateExample.substring(8, 10);    
-	    var hours = defaultDateExample.substring(11, 13);
-	    var minutes = defaultDateExample.substring(14, 16); 
-	    var seconds = defaultDateExample.substring(17, 19);
-		var dateDefault = new Date(year, month-1, day, hours, minutes, seconds, 0)
-	    
-		var displayDateTimeFormat = record.datetimeFormat.replace('HHmmss','T1');
-		displayDateTimeFormat = displayDateTimeFormat.replace('HH:mm:ss','T2');
+		var month = defaultDateExample.substring(5, 7);
+		var day = defaultDateExample.substring(8, 10);
+		var hours = defaultDateExample.substring(11, 13);
+		var minutes = defaultDateExample.substring(14, 16);
+		var seconds = defaultDateExample.substring(17, 19);
+		var dateDefault = new Date(year, month - 1, day, hours, minutes, seconds, 0)
+
+		var displayDateTimeFormat = record.datetimeFormat.replace('HHmmss', 'T1');
+		displayDateTimeFormat = displayDateTimeFormat.replace('HH:mm:ss', 'T2');
 		displayDateTimeFormat = displayDateTimeFormat.toUpperCase();
-		displayDateTimeFormat = displayDateTimeFormat.replace('T1','HHMMSS');
-		displayDateTimeFormat = displayDateTimeFormat.replace('T2','HH:MM:SS');
+		displayDateTimeFormat = displayDateTimeFormat.replace('T1', 'HHMMSS');
+		displayDateTimeFormat = displayDateTimeFormat.replace('T2', 'HH:MM:SS');
 
 		displayMessage = displayMessage.replace('{required}', convertRequiredToString(record));
 		displayMessage = displayMessage.replace('| {conditionUploadDate}', '');
@@ -117,44 +116,44 @@ module.factory('FileLayerExampleDisplayService', ['$filter', function($filter) {
 		var displayMessage = config.detailExamplePattern;
 
 		var numberFormatDisplay = 'Any numeric format'
-		if ((record.paddingCharacter != null&&record.paddingCharacter != '') || record.has1000Separator == true || record.hasDecimalPlace == true) {
+		if ((record.paddingCharacter != null && record.paddingCharacter != '') || record.has1000Separator == true || record.hasDecimalPlace == true) {
 			var numberFormatDisplay = ''
-			if(record.paddingCharacter != null && record.paddingCharacter != ''){
-				numberFormatDisplay = numberFormatDisplay + ', Padding character ('+record.paddingCharacter+')';
+			if (record.paddingCharacter != null && record.paddingCharacter != '') {
+				numberFormatDisplay = numberFormatDisplay + ', Padding character (' + record.paddingCharacter + ')';
 			}
-			if(record.has1000Separator == true){
+			if (record.has1000Separator == true) {
 				numberFormatDisplay = numberFormatDisplay + ', Has 1,000 seperator (,)';
 			}
-			if(record.hasDecimalPlace == true){
+			if (record.hasDecimalPlace == true) {
 				numberFormatDisplay = numberFormatDisplay + ', Has decimal place (.)';
 			}
 			numberFormatDisplay = numberFormatDisplay.substring(2);
 		}
-			
+
 		var signFlagTypeDisplay = '';
-		if(record.signFlagConfig !=null && !angular.isUndefined(record.signFlagConfig)){
-			signFlagTypeDisplay = ' sign flag field ('+record.signFlagConfig.displayValue+')'
-		}else if (record.hasDecimalSign == null) {
+		if (record.signFlagConfig != null && !angular.isUndefined(record.signFlagConfig)) {
+			signFlagTypeDisplay = ' sign flag field (' + record.signFlagConfig.displayValue + ')'
+		} else if (record.hasDecimalSign == null) {
 			signFlagTypeDisplay = ' within field (ignore plus symbol (+) on positive value)'
 		} else if (record.hasDecimalSign == true) {
 			signFlagTypeDisplay = ' within field (need plus symbol (+) on positive value)'
 		} else if (record.hasDecimalSign == false) {
 			signFlagTypeDisplay = ' within field (avoid plus symbol (+) on positive value)'
 		}
-		
+
 		var examplePosDataDisplay = parseFloat(config.defaultExampleValue).toFixed(record.decimalPlace);
 		var exampleNegDataDisplay = parseFloat(-config.defaultExampleValue).toFixed(record.decimalPlace);
 
 		displayMessage = displayMessage.replace('{required}', convertRequiredToString(record));
 
 		displayMessage = displayMessage.replace('{numberFormat}', numberFormatDisplay);
-		
-		if(record.decimalPlace != null && record.decimalPlace.length> 0){
+
+		if (record.decimalPlace != null && record.decimalPlace.length > 0) {
 			displayMessage = displayMessage.replace('{decimalPlace}', record.decimalPlace);
-		}else{
+		} else {
 			displayMessage = displayMessage.replace('{decimalPlace}', '0');
 		}
-		
+
 		displayMessage = displayMessage.replace('{signFlag}', signFlagTypeDisplay);
 		displayMessage = displayMessage.replace('{positiveExampleData}', examplePosDataDisplay);
 		displayMessage = displayMessage.replace('{negativeExampleData}', exampleNegDataDisplay);
@@ -182,9 +181,9 @@ module.factory('FileLayerExampleDisplayService', ['$filter', function($filter) {
 		}
 
 		var signFlagTypeDisplay = '';
-		if(record.signFlagConfig != null){
-			signFlagTypeDisplay = ' sign flag field ('+record.signFlagConfig.displayValue+')'
-		}else if (record.signFlagTypeFormat == "IGNORE_PLUS") {
+		if (record.signFlagConfig != null) {
+			signFlagTypeDisplay = ' sign flag field (' + record.signFlagConfig.displayValue + ')'
+		} else if (record.signFlagTypeFormat == "IGNORE_PLUS") {
 			signFlagTypeDisplay = ' Within field (ignore plus symbol (+) on positive value)'
 		} else if (record.signFlagTypeFormat == "NEES_PLUS") {
 			signFlagTypeDisplay = ' Within field (need plus symbol (+) on positive value)'
@@ -198,9 +197,9 @@ module.factory('FileLayerExampleDisplayService', ['$filter', function($filter) {
 		displayMessage = displayMessage.replace('{required}', convertRequiredToString(record));
 
 		displayMessage = displayMessage.replace('{numberFormat}', numberFormatDisplay);
-		if(record.decimalPlace != null){
+		if (record.decimalPlace != null) {
 			displayMessage = displayMessage.replace('{decimalPlace}', record.decimalPlace);
-		}else{
+		} else {
 			displayMessage = displayMessage.replace('{decimalPlace}', '0');
 		}
 		displayMessage = displayMessage.replace('{signFlag}', signFlagTypeDisplay);
@@ -243,22 +242,22 @@ module.factory('FileLayerExampleDisplayService', ['$filter', function($filter) {
 		} else {
 			fillerTypeMsg = 'Other';
 		}
-		
+
 		displayMessage = displayMessage.replace('{fillerType}', fillerTypeMsg);
 		displayMessage = displayMessage.replace('{exampleData}', (hasExpected ? record.expectedValue : config.defaultExampleValue));
-		if(!hasExpected){
+		if (!hasExpected) {
 			displayMessage = displayMessage.replace('(Ex. )', '');
 		}
 		return displayMessage;
 	}
-	
+
 	function SIGN_FLAG_DisplayExample(record, config) {
 		var displayMessage = config.detailExamplePattern;
 		var hasPositiveValue = !(angular.isUndefined(record.positiveFlag) || record.positiveFlag === null);
 		var hasNegativeValue = !(angular.isUndefined(record.negativeFlag) || record.negativeFlag === null);
 		var examplePosDataDisplay = parseFloat(config.defaultExampleValue);
 		var exampleNegDataDisplay = parseFloat(-config.defaultExampleValue);
-		
+
 		displayMessage = displayMessage.replace('{required}', convertRequiredToString(record));
 		displayMessage = displayMessage.replace('{positiveValue}', (hasPositiveValue ? record.positiveFlag : ''));
 		displayMessage = displayMessage.replace('{negativeValue}', (hasNegativeValue ? record.negativeFlag : ''));
@@ -269,6 +268,6 @@ module.factory('FileLayerExampleDisplayService', ['$filter', function($filter) {
 
 	function convertRequiredToString(record) {
 		return record.required == true ? 'yes' : 'no';
-	}
-	;
-} ]);
+	};
+
+}]);
