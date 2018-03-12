ed71b9725c514d71fcbf5d75a44c5c2b8a5638c9
GEC\samacharn.san <samacharn.san@gec.co.th>
2018-03-12 14:30:38 +0700

    [CHANGE] Change API name.

--- src/main/resources/static/js/app/dashboard/payment-journey/new-document.js
+++ src/main/resources/static/js/app/dashboard/payment-journey/new-document.js
@@ -1,28 +1,33 @@
-angular.module('scfApp').controller('JourneyNewDocumentPaymentController', ['$scope', 'Service', 'PageNavigation', 'SCFCommonService', '$rootScope', function($scope, Service, PageNavigation, SCFCommonService, $rootScope) {
-    var vm = this;
-    var compositParent = $scope.$parent;
-    var dashboarParent = compositParent.$parent;
-    var dahsboarItemParent = dashboarParent.dashboardItem;
-    vm.headerLabel = dahsboarItemParent.headerLabel;
+angular.module('scfApp').controller('JourneyNewDocumentPaymentController', [
+    '$scope',
+    'PageNavigation',
+    'SCFCommonService',
+    'Service',
+    function ($scope, PageNavigation, SCFCommonService, Service) {
+        var vm = this;
+        var compositParent = $scope.$parent;
+        var dashboarParent = compositParent.$parent;
+        var dahsboarItemParent = dashboarParent.dashboardItem;
+        vm.headerLabel = dahsboarItemParent.headerLabel;
 
-    vm.totalDocument = 0;
-    vm.payableAmount = '0';
-	
-    vm.load = function() {
-        var newDocumentDeferred = Service.doGet('/api/view-summary-new-ar-document');
-        newDocumentDeferred.promise.then(function(response) {
-        	
-            if(response.data != ""){
-                vm.totalDocument = response.data.totalDocument;
-			    vm.payableAmount = SCFCommonService.shortenLargeNumber(response.data.totalPayableAmount);
-            }
-        }).catch(function(response) {
-			
-        });
+        vm.totalDocument = 0;
+        vm.payableAmount = '0';
+
+        vm.load = function () {
+            var newDocumentDeferred = Service.doGet('/api/view-summary-ar-document');
+            newDocumentDeferred.promise.then(function (response) {
+
+                if (response.data != "") {
+                    vm.totalDocument = response.data.totalDocument;
+                    vm.payableAmount = SCFCommonService.shortenLargeNumber(response.data.totalPayableAmount);
+                }
+            }).catch(function (response) {
+
+            });
+        }
+
+        vm.documentList = function () {
+            PageNavigation.gotoPage('/my-organize/create-payment');
+        }
     }
-    
-    vm.documentList = function(){
-    	PageNavigation.gotoPage('/my-organize/create-payment');
-    }
-    
-}]);
+]);
