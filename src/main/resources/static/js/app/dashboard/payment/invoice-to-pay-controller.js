ac8879dee7e108eabd456953b795d587f83568e6
GEC\samacharn.san <samacharn.san@gec.co.th>
2018-03-14 19:18:31 +0700

    [FIX] Fix logo of invoice to pay (top 10) table.

--- src/main/resources/static/js/app/dashboard/payment/invoice-to-pay-controller.js
+++ src/main/resources/static/js/app/dashboard/payment/invoice-to-pay-controller.js
@@ -1,60 +1,58 @@
-angular
-		.module('scfApp')
-		.controller(
-				'InvoiceToPayDashboardController',
-				[
-						'$log',
-						'$scope',
-						'$state',
-						'$stateParams',
-						'$timeout',
-						'PageNavigation',
-						'Service',
-						'$rootScope', 
-						'$http', 
-						'$q',
-						function($log, $scope, $state, $stateParams, $timeout,
-								PageNavigation, Service, $rootScope, $http, $q) {
-							var vm = this;
-							var log = $log;
-							var organizeId = $rootScope.userInfo.organizeId;
-							
-							vm.getInvoiceToPay = function(){
-								var deffered = $q.defer();
+angular.module('scfApp').controller('InvoiceToPayDashboardController', [
+	'$http',
+	'$log',
+	'$q',
+	'$rootScope',
+	'$scope',
+	'$state',
+	'$stateParams',
+	'$timeout',
+	'PageNavigation',
+	'Service',
+	'UIFactory',
+	function ($http, $log, $q, $rootScope, $scope, $state, $stateParams, $timeout, PageNavigation, Service, UIFactory) {
+		var vm = this;
+		var log = $log;
+		var organizeId = $rootScope.userInfo.organizeId;
 
-						        $http({
-						            method: 'GET',
-						            url: 'api/v1/create-transaction/document-groupby-duedate',
-						            params: {
-						            	buyerId: organizeId
-						            }
-						        }).then(function(response){
-						            deffered.resolve(response);
-						        }).catch(function(response){
-						            deffered.reject(response);
-						        });
-						        return deffered;
-							}
-							
-							var dataSource = vm.getInvoiceToPay();
-							
-							dataSource.promise.then(function(response) {
-		                        vm.data = response.data;
-		                    }).catch();
-							
-							vm.decodeBase64 = function(data){
-								return atob(data);
-							}
-							
-							vm.create = function(data){
-								var duedate = moment(data.dueDate);
-								PageNavigation.gotoPage('/my-organize/create-payment', {
-									dashboardParams: {
-										dueDate: duedate.toDate(),
-										supplierId: data.supplierId,
-										buyerCode: data.buyerCode,
-										productType: data.productType == 'NONE' ? null : data.productType
-									}
-								})
-							}
-						} ]);
+		vm.decodeBase64 = function (data) {
+			return (data ? atob(data) :
+				UIFactory.constants.NOLOGO);
+		};
+
+		vm.getInvoiceToPay = function () {
+			var deffered = $q.defer();
+
+			$http({
+				method: 'GET',
+				url: 'api/v1/create-transaction/document-groupby-duedate',
+				params: {
+					buyerId: organizeId
+				}
+			}).then(function (response) {
+				deffered.resolve(response);
+			}).catch(function (response) {
+				deffered.reject(response);
+			});
+			return deffered;
+		}
+
+		var dataSource = vm.getInvoiceToPay();
+
+		dataSource.promise.then(function (response) {
+			vm.data = response.data;
+		}).catch();
+
+		vm.create = function (data) {
+			var duedate = moment(data.dueDate);
+			PageNavigation.gotoPage('/my-organize/create-payment', {
+				dashboardParams: {
+					dueDate: duedate.toDate(),
+					supplierId: data.supplierId,
+					buyerCode: data.buyerCode,
+					productType: data.productType == 'NONE' ? null : data.productType
+				}
+			})
+		}
+	}
+]);
