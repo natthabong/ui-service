2b166681ccb36920f7fa5cecd51a267c447d9650
GEC\atithat.dan <atithat.dan@gec.co.th>
2018-03-20 18:11:41 +0700

    commit funding profile list page

--- src/main/resources/static/js/app/modules/funding-profile/controllers/FundingProfileListController.js
+++ src/main/resources/static/js/app/modules/funding-profile/controllers/FundingProfileListController.js
@@ -4,10 +4,36 @@ fpModule.controller('FundingProfileListController',['$scope','$stateParams','UIF
 	,'PagingController','FundingProfileService','$timeout','$cookieStore','SCFCommonService','$state',
 	function($scope, $stateParams, UIFactory,PageNavigation, PagingController,FundingProfileService, 
 	$timeout,$cookieStore,SCFCommonService,$state) {
+	var vm = this;
+	vm.canManage = false;
+	
+	vm.criteria = $stateParams.criteria || { };
+	
+	// The pagingController is a tool for navigate the
+	// page of a table.
+	vm.pagingController = PagingController.create('/api/v1/fundings', vm.criteria,'GET');
+	
+	// All functions of a controller.
+	vm.search = function(pageModel) {
+		vm.pagingController.search(pageModel || ( $stateParams.backAction? {
+			offset : vm.criteria.offset,
+			limit : vm.criteria.limit
+		}: undefined));
+		$stateParams.backAction = false;
+	}
+	
+	// New Button Action
+	vm.createNew = function() {
+	}
+	
+	// Edit Button Action
+	vm.edit = function(record) {
+		
+	}
 	
 	// Main of program
 	var initLoad = function() {
-		
+		vm.search();
 	}();
 	
 }]);
