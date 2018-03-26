6bb6cf9be32569c2ee50163517dd66b4017abebc
GEC\samacharn.san <samacharn.san@gec.co.th>
2018-03-26 18:55:19 +0700

    [FIXED] Fix renaming of customer code.

--- src/main/resources/static/js/app/dashboard/transaction/newduedate-group-controller.js
+++ src/main/resources/static/js/app/dashboard/transaction/newduedate-group-controller.js
@@ -1,58 +1,54 @@
-angular
-		.module('scfApp')
-		.controller(
-				'NewDuedateGroupDashboardController',
-				[
-						'$log',
-						'$scope',
-						'$state',
-						'$stateParams',
-						'$timeout',
-						'PageNavigation',
-						'Service',
-						'$rootScope', 
-						'$http', 
-						'$q',
-						function($log, $scope, $state, $stateParams, $timeout,
-								PageNavigation, Service, $rootScope, $http, $q) {
-							var vm = this;
-							var log = $log;
-							var organizeId = $rootScope.userInfo.organizeId;
-							
-							vm.getNewDueDateGroup = function(){
-								var deffered = $q.defer();
+angular.module('scfApp').controller('NewDuedateGroupDashboardController', [
+	'$http',
+	'$log',
+	'$q',
+	'$rootScope',
+	'$scope',
+	'$state',
+	'$stateParams',
+	'$timeout',
+	'PageNavigation',
+	'Service',
+	function ($http, $log, $q, $rootScope, $scope, $state, $stateParams, $timeout, PageNavigation, Service) {
+		var vm = this;
+		var log = $log;
+		var organizeId = $rootScope.userInfo.organizeId;
 
-						        $http({
-						            method: 'GET',
-						            url: 'api/v1/create-transaction/document-groupby-sponsorpaymentdate',
-						            params: {
-						            	supplierId: organizeId
-						            }
-						        }).then(function(response){
-						            deffered.resolve(response);
-						        }).catch(function(response){
-						            deffered.reject(response);
-						        });
-						        return deffered;
-							}
-							
-							var dataSource = vm.getNewDueDateGroup();
-							
-							dataSource.promise.then(function(response) {
-		                        vm.data = response.data;
-		                    }).catch();
-							
-							vm.decodeBase64 = function(data){
-								return atob(data);
-							}
-							
-							vm.create = function(data){
-								PageNavigation.gotoPage('/my-organize/create-transaction', {
-									dashboardParams: {
-										paymentDate: data.paymentDate,
-										buyerId: data.buyerId,
-										customerCode: data.customerCode
-									}
-								})
-							}
-						} ]);
+		vm.getNewDueDateGroup = function () {
+			var deffered = $q.defer();
+
+			$http({
+				method: 'GET',
+				url: 'api/v1/create-transaction/document-groupby-sponsorpaymentdate',
+				params: {
+					supplierId: organizeId
+				}
+			}).then(function (response) {
+				deffered.resolve(response);
+			}).catch(function (response) {
+				deffered.reject(response);
+			});
+			return deffered;
+		}
+
+		var dataSource = vm.getNewDueDateGroup();
+
+		dataSource.promise.then(function (response) {
+			vm.data = response.data;
+		}).catch();
+
+		vm.decodeBase64 = function (data) {
+			return atob(data);
+		}
+
+		vm.create = function (data) {
+			PageNavigation.gotoPage('/my-organize/create-transaction', {
+				dashboardParams: {
+					paymentDate: data.paymentDate,
+					buyerId: data.buyerId,
+					customerCode: data.supplierCode
+				}
+			})
+		}
+	}
+]);
