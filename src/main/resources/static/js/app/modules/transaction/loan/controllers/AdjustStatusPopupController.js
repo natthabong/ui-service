5526b248b4bd5c75a8d35a98f4f49a95b0e4af7f
GEC\phongsathon.kon <phongsathon.kon@gec.co.th>
2018-04-11 18:07:54 +0700

    [FIXED] ปรับแก้หน้าจอ Adjust transaction กรณี มี Error

--- src/main/resources/static/js/app/modules/transaction/loan/controllers/AdjustStatusPopupController.js
+++ src/main/resources/static/js/app/modules/transaction/loan/controllers/AdjustStatusPopupController.js
@@ -1,8 +1,8 @@
 'use strict';
 angular.module('gecscf.transaction').controller(
                 'AdjustStatusPopupController',
-                [   '$scope','UIFactory','PageNavigation','ViewTransactionService','$stateParams','$timeout',
-            function($scope , UIFactory , PageNavigation , ViewTransactionService , $stateParams , $timeout) {
+                [   '$scope','UIFactory','PageNavigation','ViewTransactionService','$stateParams','$timeout','$state','$window',
+            function($scope , UIFactory , PageNavigation , ViewTransactionService , $stateParams , $timeout , $state , $window) {
                       var vm = this;
                       $scope.errors = undefined;
                       vm.transactionModel = $stateParams.transactionModel;
@@ -12,6 +12,7 @@ angular.module('gecscf.transaction').controller(
                       vm.transactionNo = $scope.ngDialogData.transactionNo;
                       vm.result  = undefined;
                       vm.reason  = $scope.ngDialogData.reason;
+                      vm.isTokenExpired = $scope.ngDialogData.isTokenExpired;
                       vm.errorMessage  = $scope.ngDialogData.errorMessage;
                       var confirmToken = undefined;
                       vm.model = {
@@ -28,16 +29,17 @@ angular.module('gecscf.transaction').controller(
                         $scope.errors = {};
                         var valid = true;
                         if (isEmpty(vm.model.password)) {
-                          $scope.errors.password = 'Password is required';
+                          $scope.errors.password = 'Password is required.';
                           valid = false;
                         }
                         if (isEmpty(vm.model.reason)) {
-                          $scope.errors.reason = 'Reason is required';
+                          $scope.errors.reason = 'Reason is required.';
                           valid = false;
                         }
                         return valid;
                       }
                       
+                    var countFail = 0;
                     vm.adjustStatus = function(callback) {
                         if (_isValid()) {
                           var preCloseCallback = function(confirm) {
@@ -64,24 +66,54 @@ angular.module('gecscf.transaction').controller(
     		                            }
     	                        });
     					  })
-    						.catch(function (response) {
+    					  .catch(function (response) {
     							console.log('Confirm status error');
-    							callback();
-    							UIFactory.showDialog({
-		                            templateUrl: '/js/app/modules/transaction/loan/templates/fail-dialog.html',
-		                            controller: 'AdjustStatusPopupController',
-		                            data: {
-		                                preCloseCallback: function(confirm) {
-		                                	init();
-		                                },
-		                                modeAdjust : vm.modeAdjust,
-		                                transactionModel : vm.transactionModel,
-		                                transactionId : transactionId,
-		                                transactionNo : response.transactionNo,
-		                                reason : response.reason,
-		                                errorMessage : response.errorMessage
-		                            }
-	                        });
+    							console.log(response);
+    							if(response.status == 403){
+    								countFail++;
+    								if(countFail < 3){
+    									$scope.errors.password = 'Invalid current password.';
+    								} else {
+    									$window.location.href = "/error/403";
+    								}
+    								
+    							} else if(response.status == 498){
+    								callback();
+        							UIFactory.showDialog({
+    		                            templateUrl: '/js/app/modules/transaction/loan/templates/fail-dialog.html',
+    		                            controller: 'AdjustStatusPopupController',
+    		                            data: {
+    		                                preCloseCallback: function(confirm) {
+    		                                	init();
+    		                                },
+    		                                modeAdjust : vm.modeAdjust,
+    		                                transactionModel : vm.transactionModel,
+    		                                transactionId : transactionId,
+    		                                transactionNo : response.transactionNo,
+    		                                reason : response.reason,
+    		                                errorMessage : response.errorMessage,
+    		                                isTokenExpired : true
+    		                            }
+        							});
+    							} else {
+    								callback();
+        							UIFactory.showDialog({
+    		                            templateUrl: '/js/app/modules/transaction/loan/templates/fail-dialog.html',
+    		                            controller: 'AdjustStatusPopupController',
+    		                            data: {
+    		                                preCloseCallback: function(confirm) {
+    		                                	init();
+    		                                },
+    		                                modeAdjust : vm.modeAdjust,
+    		                                transactionModel : vm.transactionModel,
+    		                                transactionId : transactionId,
+    		                                transactionNo : response.transactionNo,
+    		                                reason : response.reason,
+    		                                errorMessage : response.errorMessage,
+    		                                isTokenExpired : false
+    		                            }
+        							});
+    							}
     					  });
                         }
                     }
