ecc2802b38bb99964c1f9a32215c8efee8d45cd0
GEC\atithat.dan <atithat.dan@gec.co.th>
2018-04-26 13:50:39 +0700

    commit theme for organize config

--- src/main/resources/static/js/app/modules/organize/configuration/payment-date-formulas/controllers/payment-date-formula-controller.js
+++ src/main/resources/static/js/app/modules/organize/configuration/payment-date-formulas/controllers/payment-date-formula-controller.js
@@ -62,9 +62,9 @@ app.controller('PaymentDateFormulaController', [
                 label: '',
                 cssTemplate: 'text-center',
                 sortData: false,
-                cellTemplate: '<scf-button class="btn-default gec-btn-action" ng-disabled="ctrl.unauthenView() && ctrl.unauthenMangeAction()" id="payment-date-formula-{{data.formulaName}}-view-button" ng-click="ctrl.view(data)" title="View a payment date formula configs"><i class="fa fa-search" aria-hidden="true"></i></scf-button>' +
-                    '<scf-button id="payment-date-formula-{{data.formulaName}}-setup-button" ng-disabled="ctrl.unauthenMangeAction()"  class="btn-default gec-btn-action" ng-click="ctrl.config(data)" title="Config a payment date formula configs"><i class="fa fa-cog" aria-hidden="true"></i></scf-button>' +
-                    '<scf-button id="payment-date-formula-{{data.formulaName}}-delete-button" ng-disabled="ctrl.unauthenMangeAction()" class="btn-default gec-btn-action" ng-click="ctrl.deleteFormula(data)" title="Delete a payment date formula configs"><i class="fa fa-trash-o" aria-hidden="true"></i></scf-button>'
+                cellTemplate: '<scf-button class="btn btn-sm" ng-disabled="ctrl.unauthenView() && ctrl.unauthenMangeAction()" id="payment-date-formula-{{data.formulaName}}-view-button" ng-click="ctrl.view(data)" title="View a payment date formula configs"><i class="fa fa-search" aria-hidden="true"></i></scf-button>' +
+                    '<scf-button id="payment-date-formula-{{data.formulaName}}-setup-button" ng-disabled="ctrl.unauthenMangeAction()"  class="btn btn-sm" ng-click="ctrl.config(data)" title="Config a payment date formula configs"><i class="fa fa-cog" aria-hidden="true"></i></scf-button>' +
+                    '<scf-button id="payment-date-formula-{{data.formulaName}}-delete-button" ng-disabled="ctrl.unauthenMangeAction()" class="btn btn-sm" ng-click="ctrl.deleteFormula(data)" title="Delete a payment date formula configs"><i class="fa fa-trash-o" aria-hidden="true"></i></scf-button>'
             }]
         };
 
