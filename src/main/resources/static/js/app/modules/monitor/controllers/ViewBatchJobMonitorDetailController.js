633018c0ebea2ee9562c6094a3bd4bd48a70ff87
GEC\Natrada.kae <natrada.kae@gec.co.th>
2018-04-26 17:58:34 +0700

    [FIXED] changed logic for show Batch job tracking detail.

--- src/main/resources/static/js/app/modules/monitor/controllers/ViewBatchJobMonitorDetailController.js
+++ src/main/resources/static/js/app/modules/monitor/controllers/ViewBatchJobMonitorDetailController.js
@@ -1,11 +1,19 @@
 'use strict';
 var scfApp = angular.module('gecscf.monitoring');
 scfApp.controller('ViewBatchJobMonitorDetailController', 
-[ '$scope', 'Service', '$stateParams', 'UIFactory', '$q','$rootScope', '$http','PageNavigation','$timeout',
-	function($scope, Service, $stateParams, UIFactory, $q, $rootScope, $http, PageNavigation, $timeout) {
+[ '$scope', 'Service', '$stateParams', 'UIFactory', '$q','$rootScope', '$http','PageNavigation','PagingController', '$timeout',
+	'JobStatus',
+	function($scope, Service, $stateParams, UIFactory, $q, $rootScope, $http, PageNavigation, PagingController, $timeout, JobStatus) {
         var vm = this;
 
         vm.batchTrackingDetail = $stateParams.trackingDetailModel;
+        var batchTracking = $stateParams.params;
+        vm.jobStatusDropdowns = JobStatus;
+        vm.status = '';
+        
+        vm.criteria = {
+        	completed : null
+		}
         
         vm.back = function(){
             $timeout(function(){
@@ -13,11 +21,38 @@ scfApp.controller('ViewBatchJobMonitorDetailController',
             }, 10);
         }
 
+        vm.pagingController = PagingController.create('/api/v1/organizations/'+batchTracking.jobGroup+'/batch-jobs/'+batchTracking.jobId+'/logs/'+vm.batchTrackingDetail.id+'/details', vm.criteria, 'GET');
+        vm.pagingController.offsetBase = false;
+        
+        vm.searchTrackingDetail = function(pageModel) {
+        	JobStatus.forEach(function(status) {
+				if (vm.status == status.value) {
+					vm.criteria.completed = status.valueObject;
+				}
+			});
+            vm.pagingController.search(pageModel);
+	    }
+        
         var initial = function(){
-            if($stateParams.params.length == 0){
+            if(batchTracking.length == 0){
                 PageNavigation.gotoPage("/monitoring/customer-system-integration",undefined,undefined);
+            }else{
+            	vm.searchTrackingDetail();
             }
         }
         initial();
 
 } ]);
+scfApp.constant("JobStatus", [ {
+    label : 'All',
+    value : '',
+    valueObject : null
+}, {
+    label : 'Completed',
+    value : 'true',
+    valueObject : true
+}, {
+    label : 'Incomplete',
+    value : 'false',
+    valueObject : false
+}]);
